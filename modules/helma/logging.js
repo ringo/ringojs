4193a0cee4c441ae1a4fa6c37e69ef66b11c4b32
Hannes Wallnoefer <hannes@helma.at>
2008-05-15 16:14:36 +0000

    * Add some comments.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@8948 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/logging.js
+++ modules/helma/logging.js
@@ -3,6 +3,11 @@ importModule('core.string');
 var __shared__ = true;
 var configured = false;
 
+/**
+ * Configure log4j using the given file resource.
+ * Make sure to set the reset property to true in the <log4j:configuration> header
+ * e.g. <log4j:configuration xmlns:log4j='http://jakarta.apache.org/log4j/' reset="true">
+ */
 function setConfig(resource) {
     if (resource.endsWith('.properties') || resource.endsWith('.props')) {
        org.apache.log4j.PropertyConfigurator.configureAndWatch(resource);
@@ -12,6 +17,9 @@ function setConfig(resource) {
     configured = true;
 }
 
+/**
+ * Get a logger for the given name.
+ */
 function getLogger(name) {
     if (!configured) {
         org.apache.log4j.BasicConfigurator.configure();
