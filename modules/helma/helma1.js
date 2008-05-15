e7514a7544b139d3ae3e29e682bcd88a5bbf3112
Hannes Wallnoefer <hannes@helma.at>
2008-05-15 22:24:22 +0000

    * Add renderSkinAsString(), some other improvements.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@8950 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/helma1.js
+++ modules/helma/helma1.js
@@ -8,27 +8,41 @@ function createHopObject(name) {
     var ctor = proto[path[path.length - 1]] || function() {};
     proto.renderSkin = renderSkin;
     proto.addMacroHandler = addMacroHandler;
+    // proto.encode = function(str) { return str; };
     ctor.prototype = proto;
     return ctor;
 }
 
 var handlers = {};
 
-function renderSkin(name, param) {
+function renderSkin(obj, param) {
     var context = {'this': this, 'param': param};
     if (this.macroHandlers) {
         this.macroHandlers.clone(context);
     }
-    var subskin;
-    if (name.indexOf('#') > -1) {
-        [name, subskin] = name.split('#');
-    }
-    var resource = this.getResource(name + '.skin');
-    var skin = helma.skin.createSkin(resource, this);
-    if (subskin) {
-        skin = skin.getSubskin(subskin);
+    var skin;
+    if (typeof(obj.render) == "function") {
+        skin = obj;
+    } else if (typeof(obj) == "string") {
+        var subskin;
+        if (obj.indexOf('#') > -1) {
+            [obj, subskin] = obj.split('#');
+        }
+        var resource = this.getResource(obj + '.skin');
+        var skin = helma.skin.createSkin(resource, this);
+        if (subskin) {
+            skin = skin.getSubskin(subskin);
+        }
+    } else {
+        throw Error("Invalid skin object: " + obj);
     }
     helma.skin.renderSkin(skin, context, this);
+}
+
+function renderSkinAsString(obj, param) {
+    res.push();
+    this.renderSkin(obj, param);
+    return res.pop();
 }
 
 function addMacroHandler(name, handler) {
