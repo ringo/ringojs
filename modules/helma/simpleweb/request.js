d3382754fdad6aec911f52ba1df40404439ed435
Hannes Wallnoefer <hannes@helma.at>
2008-05-21 10:36:47 +0000

    * Adding req.isGet() and req.isPost() in helma.simpleweb.request submodule   as suggested by Robert Thurnher.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9002 688a9155-6ab5-4160-a077-9df41f55a9e9

---
+++ modules/helma/simpleweb/request.js
@@ -0,0 +1,15 @@
+importModule('helma.app');
+
+/**
+ * Return true if this is a HTTP POST request.
+ */
+Request.prototype.isPost = function() {
+    return this.method == "POST";
+}
+
+/**
+ * Return true if this is a HTTP GET request.
+ */
+Request.prototype.isGet = function() {
+    return this.method == "GET";
+}
