6934a613d2452c29efa7e4b29846e93a686feac9
Hannes Wallnoefer <hannes@helma.at>
2008-07-02 13:13:46 +0000

    Add Repository/Resource implementations for web application resources.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9143 688a9155-6ab5-4160-a077-9df41f55a9e9

---
+++ src/org/helma/repository/WebappRepository.java
@@ -0,0 +1,88 @@
+package org.helma.repository;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+public class WebappRepository extends AbstractRepository {
+
+    ServletContext context;
+
+    long timestamp;
+
+    public WebappRepository(ServletContext context, String path) {
+        this.context = context;
+        this.parent = null;
+        if (path == null) {
+            path = "/";
+        } else if (!path.endsWith("/")) {
+            path = path + "/";
+        }
+        this.path = path;
+        this.name = path;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    protected WebappRepository(ServletContext context, WebappRepository parent, String name) {
+        this.context = context;
+        this.parent = parent;
+        this.name = name;
+        this.path = parent.path + name + "/";
+        this.timestamp = parent.timestamp;
+    }
+
+    public long getChecksum() throws IOException {
+        return timestamp;
+    }
+
+    public long lastModified() throws IOException {
+        return timestamp;
+    }
+
+    public boolean exists() {
+        if ("/".equals(path)) {
+            return true;
+        }
+        Set paths = context.getResourcePaths(path);
+        return (paths != null && !paths.isEmpty());
+    }
+
+    public Repository getChildRepository(String name) {
+        return new WebappRepository(context, this, name);
+    }
+
+    public void update() {
+        Set paths = context.getResourcePaths(path);
+        if (paths != null) {
+
+            ArrayList<Repository> newRepositories = new ArrayList<Repository>(paths.size());
+            HashMap<String,Resource> newResources = new HashMap<String,Resource>(paths.size());
+
+            for (Object obj: paths) {
+                String path = (String) obj;
+                if (path.endsWith("/")) {
+                    int n = path.lastIndexOf('/', path.length() - 2);
+                    String name = path.substring(n + 1, path.length() - 1);
+                    newRepositories.add(new WebappRepository(context, this, name));
+                } else {
+                    int n = path.lastIndexOf('/', path.length() - 1);
+                    String name = path.substring(n + 1);
+                    newResources.put(name, new WebappResource(context, this, name));
+                }
+            }
+
+            repositories = newRepositories.toArray(new Repository[newRepositories.size()]);
+            resources = newResources;
+
+        } else {
+            repositories = emptyRepositories;
+            resources = new HashMap<String,Resource>();
+        }
+    }
+
+    protected Resource createResource(String name) {
+        return new WebappResource(context, this, name);
+    }
+}
---
+++ src/org/helma/repository/WebappResource.java
@@ -0,0 +1,48 @@
+package org.helma.repository;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class WebappResource extends AbstractResource {
+
+    ServletContext context;
+
+    protected WebappResource(ServletContext context, WebappRepository repository, String name) {
+        this.context = context;
+        this.repository = repository;
+        this.name = name;
+        this.path = repository.getPath() + name;
+        setBaseNameFromName(name);
+    }
+
+    public long lastModified() throws IOException {
+        return repository.lastModified();
+    }
+
+    public boolean exists() {
+        try {
+            return context.getResource(path) != null;
+        } catch (MalformedURLException mux) {
+            return false;
+        }
+    }
+
+    public long getLength() throws IOException {
+        return 0;
+    }
+
+    public InputStream getInputStream() throws IOException {
+        return context.getResourceAsStream(path);
+    }
+
+    public URL getUrl() throws MalformedURLException {
+        return context.getResource(path);
+    }
+
+    public Repository getRepository() {
+        return repository;
+    }
+}
