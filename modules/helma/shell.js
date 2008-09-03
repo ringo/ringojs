c799f9eedb6f491b578f5ae30e18ddad08a1c324
Hannes Wallnoefer <hannes@helma.at>
2008-09-03 23:30:32 +0000

    Add write() method to helma.shell module.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9179 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/shell.js
+++ modules/helma/shell.js
@@ -1,17 +1,25 @@
 // This module is evaluated on shell scopes,
 // so to extend the helma shell just add stuff here.
 
+var out = java.lang.System.out;
+
 /**
- * Write 0..n arguments to standard out.
+ * Write 0..n arguments to standard output.
  */
-function writeln() {
+function write() {
     var length = arguments.length;
-    var out = java.lang.System.out;
     for (var i = 0; i < length; i++) {
         out.print(String(arguments[i]));
         if (i < length - 1)
             out.print(' ');
-    } 
+    }
+}
+
+/**
+ * Write 0..n arguments to standard output, followed by a newline.
+ */
+function writeln() {
+    write.apply(this, arguments);
     out.println();
 }
 
