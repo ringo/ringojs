d0c90a5790b523dd51f3c8bfee13321f8641827f
Hannes Wallnoefer <hannes@helma.at>
2008-09-22 10:35:57 +0000

    Do not use Arrays.copyOfRange() as it is Java 6 only. Include script name as first element of script args.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9229 688a9155-6ab5-4160-a077-9df41f55a9e9

--- src/org/helma/tools/HelmaRunner.java
+++ src/org/helma/tools/HelmaRunner.java
@@ -49,10 +49,9 @@ public class HelmaRunner {
                 }
             }
             if (i < args.length) {
-            	scriptName = args[i++];
-                if (i < args.length) {
-                	scriptArgs = Arrays.copyOfRange(args, i, args.length);
-                }
+            	scriptName = args[i];
+                scriptArgs = new String[args.length - i];
+                System.arraycopy(args, i, scriptArgs, 0, scriptArgs.length);
             }
         }
 
