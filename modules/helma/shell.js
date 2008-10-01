39f0e7f93647f29f4c8a35d345c71add70e4c5ee
Hannes Wallnoefer <hannes@helma.at>
2008-10-01 10:19:53 +0000

    Implement read() and readln() function in shell module.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9263 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/shell.js
+++ modules/helma/shell.js
@@ -1,7 +1,8 @@
 // This module is evaluated on shell scopes,
 // so to extend the helma shell just add stuff here.
 
-var out = java.lang.System.out;
+var output = java.lang.System.out;
+var input = new java.io.InputStreamReader(java.lang.System["in"]);
 
 /**
  * Write 0..n arguments to standard output.
@@ -9,9 +10,9 @@ var out = java.lang.System.out;
 function write() {
     var length = arguments.length;
     for (var i = 0; i < length; i++) {
-        out.print(String(arguments[i]));
+        output.print(String(arguments[i]));
         if (i < length - 1)
-            out.print(' ');
+            output.print(' ');
     }
 }
 
@@ -20,7 +21,32 @@ function write() {
  */
 function writeln() {
     write.apply(this, arguments);
-    out.println();
+    output.println();
+}
+
+/**
+ * Read a single character from the standard input.
+ */
+function read() {
+    return String.fromCharCode(input.read());
+}
+
+/**
+ * Read a single line from the standard input. 
+ * @param password {Boolean} if true character echo is replaced with '*'
+ */
+function readln(password) {
+    var buffer = [];
+    var c;
+    while ((c = read()) != '\r' && c != '\n') {
+        if (password) {
+            write('*');
+        } else {
+            write(c);
+        }
+        buffer.push(c);
+    }
+    return buffer.join('');
 }
 
 /**
