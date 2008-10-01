ecf6e937ecb35f3093c3d161e40b61a3c560a1aa
Hannes Wallnoefer <hannes@helma.at>
2008-10-01 13:54:04 +0000

    Add experimental http parameter grouping support. Grouped params are in req.grouped and are handled using dot syntax rather than square bracket syntax as in Helma 1.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9265 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/webapp/request.js
+++ modules/helma/webapp/request.js
@@ -1,3 +1,5 @@
+loadModule('core.string');
+var log = loadModule('helma.logging').getLogger(__name__);
 var system = loadModule('helma.system');
 system.addHostObject(org.helma.web.Request);
 system.addHostObject(org.helma.web.Session);
@@ -14,4 +16,73 @@ Request.prototype.isPost = function() {
  */
 Request.prototype.isGet = function() {
     return this.method == "GET";
+}
+
+// experimental parameter grouping
+
+Request.prototype.__defineGetter__("grouped",
+        function() {
+            return new ParameterGroup("", this.data);
+        });
+
+function ParameterGroup(path, collection) {
+
+    var props;
+
+    function initProps() {
+        props = {};
+        for (var i in collection) {
+            if (i.startsWith(path)) {
+                var dot = i.indexOf('.', path.length);
+                var key, value;
+                if (dot > -1) {
+                    key = i.slice(path.length, dot);
+                    value = new ParameterGroup(i.slice(0, dot + 1), collection);
+                } else  {
+                    key = i.slice(path.length);
+                    value = collection[i];
+                }
+                if (!(key in props)) {
+                    props[key] = value;
+                }
+            }
+        }
+    }
+
+    this.__iterator__ = function() {
+        if (!props) {
+            initProps();
+        }
+        for (var [key, value] in props) {
+            yield [key, value];
+        }
+        throw StopIteration;
+    };
+
+    this.__get__ = function(name) {
+        if (!props) {
+            initProps();
+        }
+        return props[name];
+    };
+
+    this.__has__ = function(name) {
+        if (!props) {
+            initProps();
+        }
+        return name in props || name in this;
+    };
+
+    this.__getIds__ = function() {
+        var ids = [];
+        if (!props) {
+            initProps();
+        }
+        for (var key in props) {
+            ids.push(key);
+        }
+        return ids;
+    }
+
+    return this;
 }
