b7e6629202837adfa8d6366c6bc05ea0ba3e8dfd
Hannes Wallnoefer <hannes@helma.at>
2008-10-13 14:53:33 +0000

    Implement HTTP parameter grouping without metamethod magic, resulting in much simpler code.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9288 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/webapp/request.js
+++ modules/helma/webapp/request.js
@@ -27,51 +27,21 @@ Request.prototype.__defineGetter__("grouped",
 
 function ParameterGroup(path, collection) {
 
-    var props;
-
-    function initProps() {
-        props = {};
-        for (var i in collection) {
-            if (i.startsWith(path)) {
-                var dot = i.indexOf('.', path.length);
-                var key, value;
-                if (dot > -1) {
-                    key = i.slice(path.length, dot);
-                    value = new ParameterGroup(i.slice(0, dot + 1), collection);
-                } else  {
-                    key = i.slice(path.length);
-                    value = collection[i];
-                }
-                if (!(key in props)) {
-                    props[key] = value;
-                }
+    for (var i in collection) {
+        if (i.startsWith(path)) {
+            var dot = i.indexOf('.', path.length);
+            var key, value;
+            if (dot > -1) {
+                key = i.slice(path.length, dot);
+                value = new ParameterGroup(i.slice(0, dot + 1), collection);
+            } else  {
+                key = i.slice(path.length);
+                value = collection[i];
+            }
+            if (!this.hasOwnProperty(key)) {
+                this[key] = value;
             }
         }
-    }
-
-    this.__get__ = function(name) {
-        if (!props) {
-            initProps();
-        }
-        return props[name];
-    };
-
-    this.__has__ = function(name) {
-        if (!props) {
-            initProps();
-        }
-        return name in props || name in this;
-    };
-
-    this.__getIds__ = function() {
-        var ids = [];
-        if (!props) {
-            initProps();
-        }
-        for (var key in props) {
-            ids.push(key);
-        }
-        return ids;
     }
 
     return this;
