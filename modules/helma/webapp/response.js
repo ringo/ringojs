43ccb7a5fabbf409b2f7a89db6b588e40eb4d0ea
Hannes Wallnoefer <hannes@helma.at>
2008-12-10 15:22:35 +0000

    Remove old style system callback, add function names for render() and debug()
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9403 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/webapp/response.js
+++ modules/helma/webapp/response.js
@@ -2,19 +2,13 @@ import('helma.system', 'system');
 
 system.addHostObject(org.helma.web.Response);
 
-system.addCallback("onResponse", "debugFlusher", function(res) {
-    if (res.status == 200 || res.status >= 400) {
-        res.flushDebug();
-    }
-})
-
 /**
  * Render a skin to the response's buffer
  * @param skin
  * @param context
  * @param scope
  */
-Response.prototype.render = function(skin, context, scope) {
+Response.prototype.render = function render(skin, context, scope) {
     var render = require('helma.skin').render;
     this.write(render(skin, context, scope));
 }
@@ -22,7 +16,7 @@ Response.prototype.render = function(skin, context, scope) {
 /**
  * Print a debug message to the rendered page.
  */
-Response.prototype.debug = function() {
+Response.prototype.debug = function debug() {
     var buffer = this.debugBuffer || new Buffer();
     buffer.write("<div class=\"helma-debug-line\" style=\"background: yellow;");
     buffer.write("color: black; border-top: 1px solid black;\">");
