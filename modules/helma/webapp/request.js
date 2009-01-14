cbc939c94a3ab53b7950437600a10f7cff0cd62f
Hannes Wallnoefer <hannes@helma.at>
2009-01-14 13:36:04 +0000

    Remove complex getter code - this stuff is now implemented in Java (ParameterMap)
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9481 688a9155-6ab5-4160-a077-9df41f55a9e9

--- modules/helma/webapp/request.js
+++ modules/helma/webapp/request.js
@@ -8,44 +8,6 @@ var log = require('helma.logging').getLogger(__name__);
 
 (function() {
 
-    function cachedGetter(name, fn) {
-        return function() {
-            // make sure to never set anything on request prototype
-            if (this != Request.prototype) {
-                var cache = this.__property_cache__;
-                if (!cache) {
-                    cache = this.__property_cache__ = {};
-                }
-                if (!cache[name]) {
-                    cache[name] = fn.apply(this);
-                }
-                return cache[name];
-            }
-        };
-    }
-
-    function ParameterGroup(path, map) {
-
-        for (var i in map) {
-            if (i.startsWith(path)) {
-                var dot = i.indexOf('.', path.length);
-                var key, value;
-                if (dot > -1) {
-                    key = i.slice(path.length, dot);
-                    value = new ParameterGroup(i.slice(0, dot + 1), map);
-                } else {
-                    key = i.slice(path.length);
-                    value = map[i];
-                }
-                if (!this.hasOwnProperty(key)) {
-                    this[key] = value[0];
-                }
-            }
-        }
-
-        return this;
-    }
-
     /**
      * Return true if this is a HTTP POST request.
      */
@@ -59,25 +21,5 @@ var log = require('helma.logging').getLogger(__name__);
     this.isGet = function() {
         return this.method == "GET";
     }
-
-    /* this.__defineGetter__("cookies", cachedGetter('cookies',
-        function() {
-            var cookies = {};
-            for each (var cookie in this.getCookies()) {
-                cookies[cookie.name] = cookie.value;
-            }
-            return cookies;
-        })
-    ); */
-
-    /* this.__defineGetter__("params", cachedGetter('params', function() {
-            return new ParameterGroup("", this.getParameterMap());
-        })
-    ); */
-
-    this.__defineGetter__("data", cachedGetter('data', function() {
-            return new ParameterGroup("", this.getParameterMap());
-        })
-    );
  
 }).apply(Request.prototype);
