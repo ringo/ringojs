b786df06d5712b93d837f049bc06be16d202cb84
Hannes Wallnoefer <hannes@helma.at>
2009-03-20 13:41:45 +0000

    Add new helma/util module containing utility functions for generating property descriptors for use in Object.defineProperty() that map to a property of another object.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9554 688a9155-6ab5-4160-a077-9df41f55a9e9

---
+++ modules/helma/util.js
@@ -0,0 +1,60 @@
+
+export('readOnlyPropertyDesc', 'writeOnlyPropertyDesc', 'readWritePropertyDesc');
+
+/**
+ * Create a read-only property descriptor to be used as third argument in
+ * Object.defineProperty that maps a property to a property in
+ * another object.
+ *
+ * The third argument is optional and can be used to define additional
+ * settings on the descriptor such as enumerable, writable, or configurable.
+ *
+ * @param obj the target object
+ * @param name the target property name
+ * @param desc optional: the descriptor object, or undefined
+ * @return a property descriptor object that maps to a property in a target object
+ */
+function readOnlyPropertyDesc(obj, name, desc) {
+    desc = desc || {};
+    desc.getter = function() { return obj[name]; }
+    return desc;
+}
+
+/**
+ * Create a write-only property descriptor to be used as third argument in
+ * Object.defineProperty that maps a property to a property in
+ * another object.
+ *
+ * The third argument is optional and can be used to define additional
+ * settings on the descriptor such as enumerable, writable, or configurable.
+ *
+ * @param obj the target object
+ * @param name the target property name
+ * @param desc optional: the descriptor object, or undefined
+ * @return a property descriptor object that maps to a property in a target object
+ */
+function writeOnlyPropertyDesc(obj, name, desc) {
+    desc = desc || {};
+    desc.setter = function(value) { obj[name] = value; }
+    return desc;
+}
+
+/**
+ * Create a read-write property descriptor to be used as third argument in
+ * Object.defineProperty that maps a property to a property in
+ * another object.
+ *
+ * The third argument is optional and can be used to define additional
+ * settings on the descriptor such as enumerable, writable, or configurable.
+ *
+ * @param obj the target object
+ * @param name the target property name
+ * @param desc optional: the descriptor object, or undefined
+ * @return a property descriptor object that maps to a property in a target object
+ */
+function readWritePropertyDesc(obj, name, desc) {
+    desc = desc || {};
+    desc.getter = function() { return obj[name]; }
+    desc.setter = function(value) { obj[name] = value; }
+    return desc;
+}
