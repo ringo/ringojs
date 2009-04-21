7d86f9151de13bb7a24b1f28e6a4733bfca3e6de
Hannes Wallnoefer <hannes@helma.at>
2009-04-21 16:26:49 +0000

    Print message and exit if jack libs can't be loaded.
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9642 688a9155-6ab5-4160-a077-9df41f55a9e9

--- apps/jack/main.js
+++ apps/jack/main.js
@@ -1,10 +1,22 @@
-var http = require('helma/httpserver');
-http.start({
-    port: 8080,
-    moduleName: 'jack/narwhal',
-    functionName: 'Narwhal'
-});
+try {
 
-// Alternatively, you can pass the jack app to start() as second argument.
-// this bypasses module loading in the servlet, so changes in the Jack app won't be picked up.
-// http.start({ port: 8080 }, require('jack/narwhal').Narwhal);
+    // fail fast if no jack libs on the module path
+    require('jack/narwhal');
+
+    var http = require('helma/httpserver');
+    http.start({
+        port: 8080,
+        moduleName: 'jack/narwhal',
+        functionName: 'Narwhal'
+    });
+
+    // Alternatively, you can pass the jack app to start() as second argument.
+    // this bypasses module loading in the servlet, so changes in the Jack app won't be picked up.
+    // http.start({ port: 8080 }, require('jack/narwhal').Narwhal);
+
+} catch (error) {
+    print();
+    print("Can't start Jack demo. Make sure you have Jack installed and configured as described in README.txt.");
+    print();
+    print(error);
+}
