0f90b8ad019e3ec6224d38b97934f89c1ec8febd
Andreas Bolka <andreas.bolka@gmx.net>
2009-05-18 14:25:21 +0000

    Refactor create-app script to an admin module
    The separate script is no longer needed, applications can now be created
    with `helma admin/create <appname>`.
    
    Implements #33.
    
    From: Andreas Bolka <andreas.bolka@gmx.net>
    
    git-svn-id: https://dev.helma.org/svn/helma-ng/trunk@9803 688a9155-6ab5-4160-a077-9df41f55a9e9

---
+++ modules/admin/create.js
@@ -0,0 +1,63 @@
+require('core/string');
+include('helma/system');
+include('helma/file');
+include('helma/shell');
+
+export('createApplication');
+
+function createApplication(path) {
+    if (!path) {
+        throw "No destination path given.";
+    }
+    var dest = new File(path);
+
+    if (dest.exists() && !dest.isDirectory()) {
+        throw dest + " exists but is not a directory.";
+    } else if (dest.isDirectory() && dest.list().length > 0) {
+        throw "Directory " + dest + " exists but is not empty.";
+    }
+
+    var skeleton = new File(properties["helma.home"], "apps/skeleton");
+    if (!skeleton.exists() || !skeleton.isDirectory()) {
+        throw "Can't find skeleton app in " + skeleton + ".";
+    }
+
+    write("Copying application skeleton to", dest, "... ");
+    skeleton.hardCopy(dest);
+    print("done");
+}
+
+function main(args) {
+    if (args[1] && args[1].startsWith("-")) {
+        switch (args[1]) {
+            case "-h":
+            case "--help":
+                print("Creates a new Helma NG application");
+                print("Usage:");
+                print("  helma admin/create [path]");
+                break;
+            default:
+                print("Unknown option", args[1]);
+                break;
+        }
+        quit();
+    }
+
+    var path = args[1]
+            || readln("Please enter the directory path for your application: ");
+
+    if (!path) {
+        print("No path, exiting.");
+    } else {
+        createApplication(path);
+    }
+}
+
+if (__name__ == "__main__") {
+    try {
+        main(args);
+    } catch (err) {
+        print("Error: " + err);
+        print("Exiting.");
+    }
+}
