49229bcf20539eb47b7ef650e2117cd3f353e023
Robert Gaggl <robert@nomatic.org>
2009-06-11 13:02:07 +0200

    run() now accepts either an (exports) object or a module path. this allows starting the runner from within a test module (see test/all.js)

--- modules/test/all.js
+++ modules/test/all.js
@@ -1,8 +1,11 @@
-import("helma/unittest");
-
+exports.unittest = require("test/helma/unittest_test");
 exports.array = require("test/core/array_test");
 exports.object = require("test/core/object_test");
 exports.file = require("test/helma/file_test");
 exports.skin = require("test/helma/skin_test");
-exports.unittest = require("test/helma/unittest_test");
 exports.narwhal = require("test/narwhal/io_test");
+
+// start the test runner if we're called directly from command line
+if (__name__ == "__main__") {
+    require("helma/unittest").run(exports);
+}
