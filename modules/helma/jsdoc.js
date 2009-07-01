bee5876ab650b3ab5a070dab0285cd225407af2e
Hannes Wallnöfer <hannesw@gmail.com>
2009-07-01 15:18:00 +0200

    Add some JSDoc parsing smarts, partially borrowed from jsdoc-toolkit, partially self-grown.

--- modules/helma/jsdoc.js
+++ modules/helma/jsdoc.js
@@ -37,10 +37,54 @@ exports.parseScriptResource = function(resource, visitorFunction) {
     }));
 }
 
+/**
+ * Remove slash-star comment wrapper from a raw comment string.
+ * @type String
+ */
+exports.unwrapComment = function(/**String*/comment) {
+    return comment ? comment.replace(/(^\/\*\*|\*\/$)/g, "").replace(/^\s*\* ?/gm, "") : "";
+}
+
+/**
+ * Parse a JSDoc comment into an array of tags, with tags being represented
+ * as [tagname, tagtext].
+ * @param {String} comment the raw JSDoc comment
+ * @return {Array} an array of tags.
+ */
+exports.extractTags = function(/**String*/comment) {
+    if (comment.startsWith("/**")) {
+        comment = exports.unwrapComment(comment);
+    }
+    var tags = comment.split(/(^|[\r\n])\s*@/)
+            .filter(function($){return $.match(/\S/)});
+    return tags.map(function(tag, idx) {
+        if (idx == 0 && !comment.startsWith('@')) {
+            return ['desc', tag.trim()];
+        } else {
+            var space = tag.search(/\s/);
+            return space > -1 ?
+                   [tag.substring(0, space), tag.substring(space + 1).trim()] :
+                   [tag, ''];
+        }
+    })
+}
+
+/**
+ * Utility function to test whether a node is a Name node
+ * (a node of type org.mozilla.javascript.ast.Name)
+ * @param node {Object} an AST node
+ * @return {Boolean} true if node is a name node
+ */
 exports.isName = function(node) {
     return node instanceof org.mozilla.javascript.ast.Name;
 }
 
+/**
+ * Utility function to get the name value of a node, or the empty
+ * string if it is not a name node.
+ * @param node an AST node
+ * @return {String} the name value of the node
+ */
 exports.getName = function(node) {
     return exports.isName(node) ? node.getString() : "";
 }
