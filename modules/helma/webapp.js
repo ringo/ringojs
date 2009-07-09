403a353a9de28deba19ed84ad83ce59326ee2d1b
Hannes Wallnöfer <hannesw@gmail.com>
2009-07-09 14:49:58 +0200

    Unwrap nested functions in helma/webapp.handleRequest()

--- modules/helma/webapp.js
+++ modules/helma/webapp.js
@@ -45,100 +45,9 @@ function handleRequest(env) {
     // used to compose req.actionPath, which is the part of the path
     // that resolves to the action (req.path minus argument elements)
     var actionPath = ["/"];
-
-    function getPattern(spec) {
-        var pattern = spec[0];
-        if (typeof pattern == "string") {
-            if (pattern.startsWith("/"))
-                pattern = pattern.replace("/", "^");
-            pattern = spec[0] = new RegExp(pattern);
-        } else if (!(pattern instanceof RegExp)) {
-            throw Error("Pattern must be a regular expression or string");
-        }
-        return pattern;
-    }
-
-    function getModule(spec, prefix) {
-        var module = spec[1];
-        if (typeof module == "string") {
-            module = require(prefix + module);
-        } else if (!(module instanceof Object)) {
-            throw Error("Module must be a string or object");
-        }
-        return module;
-    }
-
-    function getAction(module, name) {
-        name = name || "index";
-        var action = module[name.replace(/\./g, "_")];
-        if (typeof action == "function") {
-            return action;
-        }
-        return null;
-    }
-
-    function resolveInConfig(config, path, prefix) {
-        if (log.isDebugEnabled) log.debug('resolving path ' + path);
-        if (config.urls instanceof Array) {
-            for each (var url in config.urls) {
-                if (log.isDebugEnabled) log.debug("checking url line: " + url);
-                var match = getPattern(url).exec(path);
-                if (log.isDebugEnabled) log.debug("got match: " + match);
-                if (match != null) {
-                    var module = getModule(url, prefix);
-                    if (log.isDebugEnabled) log.debug("module: " + module);
-                    // cut matching prefix from path
-                    path = path.substring(match[0].length);
-                    //remove leading and trailing slashes and split
-                    var pathArray = path.replace(/^\/+|\/+$/g, "").split(/\/+/);
-                    var action = getAction(module, pathArray[0]);
-                    // log.debug("got action: " + action);
-                    if (typeof action == "function" && pathArray.length <= action.length) {
-                        // default action - make sure request path has trailing slash
-                        if (!pathArray[0]) {
-                            req.checkTrailingSlash() 
-                        }
-                        // set req.actionPath to the part of the path that resolves to the action
-                        actionPath.push(match[0], pathArray[0] || "index");
-                        req.actionPath =  actionPath.join("/").replace(/\/+/g, "/");
-                        // add remaining path elements as additional action arguments
-                        var actionArgs = pathArray.slice(1);
-                        var matchedArgs = match.slice(1);
-                        var args = [req].concat(matchedArgs).concat(actionArgs);
-                        var middleware = config.middleware;
-                        var middlewareIndex = 0;
-                        // set up middleware chain in request object
-                        req.process = function() {
-                            if (middleware instanceof Array
-                                    && middlewareIndex < middleware.length) {
-                                return invokeMiddleware(middleware[middlewareIndex++], [req]);
-                            } else {
-                                var res = action.apply(module, args);
-                                if (res && typeof res === 'object'
-                                        && typeof res.close === 'function') {
-                                    return res.close();
-                                }
-                                return res;
-                            }
-                        }
-                        return req.process();
-                    } else if (module.urls instanceof Array) {
-                        // nested app - make sure request path has trailing slash
-                        if (!path) {
-                            req.checkTrailingSlash();
-                        }
-                        actionPath.push(match[0]);
-                        return resolveInConfig(module, path, match[0] + "/");
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
+    
     try {
-        res = resolveInConfig(config, decodeURI(path), "");
+        res = resolveInConfig(req, config, actionPath, decodeURI(path), "");
     } catch (e) {
         if (e.retry) {
             throw e;
@@ -156,6 +65,99 @@ function handleRequest(env) {
     }
     return res;
 }
+
+function resolveInConfig(req, config, actionPath, path, prefix) {
+    if (log.isDebugEnabled) log.debug('resolving path ' + path);
+    if (config.urls instanceof Array) {
+        for each (var url in config.urls) {
+            if (log.isDebugEnabled) log.debug("checking url line: " + url);
+            var match = getPattern(url).exec(path);
+            if (log.isDebugEnabled) log.debug("got match: " + match);
+            if (match != null) {
+                var module = getModule(url, prefix);
+                if (log.isDebugEnabled) log.debug("module: " + module);
+                // cut matching prefix from path
+                path = path.substring(match[0].length);
+                //remove leading and trailing slashes and split
+                var pathArray = path.replace(/^\/+|\/+$/g, "").split(/\/+/);
+                var action = getAction(module, pathArray[0]);
+                // log.debug("got action: " + action);
+                if (typeof action == "function" && pathArray.length <= action.length) {
+                    // default action - make sure request path has trailing slash
+                    if (!pathArray[0]) {
+                        req.checkTrailingSlash()
+                    }
+                    // set req.actionPath to the part of the path that resolves to the action
+                    actionPath.push(match[0], pathArray[0] || "index");
+                    req.actionPath =  actionPath.join("/").replace(/\/+/g, "/");
+                    // add remaining path elements as additional action arguments
+                    var actionArgs = pathArray.slice(1);
+                    var matchedArgs = match.slice(1);
+                    var args = [req].concat(matchedArgs).concat(actionArgs);
+                    var middleware = config.middleware;
+                    var middlewareIndex = 0;
+                    // set up middleware chain in request object
+                    req.process = function() {
+                        if (middleware instanceof Array
+                                && middlewareIndex < middleware.length) {
+                            return invokeMiddleware(middleware[middlewareIndex++], [req]);
+                        } else {
+                            var res = action.apply(module, args);
+                            if (res && typeof res === 'object'
+                                    && typeof res.close === 'function') {
+                                return res.close();
+                            }
+                            return res;
+                        }
+                    }
+                    return req.process();
+                } else if (module.urls instanceof Array) {
+                    // nested app - make sure request path has trailing slash
+                    if (!path) {
+                        req.checkTrailingSlash();
+                    }
+                    actionPath.push(match[0]);
+                    return resolveInConfig(req, module, actionPath, path, match[0] + "/");
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+function getPattern(spec) {
+    var pattern = spec[0];
+    if (typeof pattern == "string") {
+        if (pattern.startsWith("/"))
+            pattern = pattern.replace("/", "^");
+        pattern = spec[0] = new RegExp(pattern);
+    } else if (!(pattern instanceof RegExp)) {
+        throw Error("Pattern must be a regular expression or string");
+    }
+    return pattern;
+}
+
+function getModule(spec, prefix) {
+    var module = spec[1];
+    if (typeof module == "string") {
+        module = require(prefix + module);
+    } else if (!(module instanceof Object)) {
+        throw Error("Module must be a string or object");
+    }
+    return module;
+}
+
+function getAction(module, name) {
+    name = name || "index";
+    var action = module[name.replace(/\./g, "_")];
+    if (typeof action == "function") {
+        return action;
+    }
+    return null;
+}
+
 
 function invokeMiddleware(middleware, args) {
     var functionName = 'handleRequest';
