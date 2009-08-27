c01ecd4aff44820cb515479ea709319012adaa85
Hannes Wallnöfer <hannesw@gmail.com>
2009-08-27 23:33:34 +0200

    Use longer file for io read test.

--- modules/test/io_test.js
+++ modules/test/io_test.js
@@ -10,7 +10,7 @@ exports.testReadFixed = function() {
 };
 
 exports.testReadIndefinite = function() {
-    var resource = getResource('test/io_test.js');
+    var resource = getResource('test/helma/unittest_test.js');
     var io = new IOStream(resource.inputStream);
     var bytes = io.read();
     assertEqual(bytes.length, resource.length);
