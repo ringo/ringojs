843327ed335760816098c9bd4a4e227c5985ed3f
Hannes Wallnöfer <hannesw@gmail.com>
2009-09-02 16:53:24 +0200

    Add more tests to test/all

--- modules/test/all.js
+++ modules/test/all.js
@@ -6,6 +6,10 @@ exports.skin = require("./helma/skin_test");
 exports.fileIterator = require("./file/iterator");
 exports.fileNormal = require("./file/normal");
 exports.fileResolve = require("./file/resolve");
+exports.fileAbsolute = require("./file/is-absolute");
+exports.fileDirname = require("./file/dirname");
+exports.fileExtension = require("./file/extension");
+
 exports.io = require("./io_test");
 
 // start the test runner if we're called directly from command line
