c1c96d4a4fba042a53309237d9e824a9eb531baf
Hannes Wallnöfer <hannesw@gmail.com>
2009-09-03 21:05:00 +0200

    Be more specific in ByteArray.slice() tests

--- modules/test/binary/bytearray-slice.js
+++ modules/test/binary/bytearray-slice.js
@@ -12,43 +12,58 @@ exports.testByteArraySlice = function() {
 
     s = b.slice(3, 6);
     assertTrue(s instanceof ByteArray);
+    assertEqual(3, s.length);
     assertEqual(a.slice(3, 6), s.toArray());
 
     s = b.slice(3, 4);
     assertTrue(s instanceof ByteArray);
+    assertEqual(1, s.length);
     assertEqual(a.slice(3, 4), s.toArray());
 
     s = b.slice(3, 3);
     assertTrue(s instanceof ByteArray);
+    assertEqual(0, s.length);
     assertEqual(a.slice(3, 3), s.toArray());
 
     s = b.slice(3, 2);
     assertTrue(s instanceof ByteArray);
+    assertEqual(0, s.length);
     assertEqual(a.slice(3, 2), s.toArray());
 
     s = b.slice(7);
     assertTrue(s instanceof ByteArray);
+    assertEqual(3, s.length);
     assertEqual(a.slice(7), s.toArray());
 
     s = b.slice(3, -2);
     assertTrue(s instanceof ByteArray);
+    assertEqual(5, s.length);
     assertEqual(a.slice(3, -2), s.toArray());
 
     s = b.slice(-2);
     assertTrue(s instanceof ByteArray);
+    assertEqual(2, s.length);
     assertEqual(a.slice(-2), s.toArray());
 
     s = b.slice(50);
     assertTrue(s instanceof ByteArray);
+    assertEqual(0, s.length);
     assertEqual(a.slice(50), s.toArray());
 
     s = b.slice(-100, 100);
     assertTrue(s instanceof ByteArray);
+    assertEqual(10, s.length);
     assertEqual(a.slice(-100, 100), s.toArray());
 
     s = b.slice("foo");
     assertTrue(s instanceof ByteArray);
+    assertEqual(10, s.length);
     assertEqual(a.slice("foo"), s.toArray());
+
+    s = b.slice("foo", "bar");
+    assertTrue(s instanceof ByteArray);
+    assertEqual(0, s.length);
+    assertEqual(a.slice("foo", "bar"), s.toArray());
 };
 
 if (require.main === module.id) {
