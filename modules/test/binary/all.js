60cafd3349f8b90ed4ee8f8789a0f89016d4ac8c
Hannes Wallnöfer <hannesw@gmail.com>
2009-09-04 12:37:05 +0200

    Fix duplicate export names

--- modules/test/binary/all.js
+++ modules/test/binary/all.js
@@ -1,6 +1,6 @@
 
-exports.bytearrayEncodings = require('./bytearray-tests');
+exports.bytearray = require('./bytearray-tests');
 exports.bytearrayEncodings = require('./bytearray-encodings-tests');
-exports.bytestringEncodings = require('./bytestring-tests');
+exports.bytestring = require('./bytestring-tests');
 exports.bytestringEncodings = require('./bytestring-encodings-tests');
 exports.bytearraySlice = require('./bytearray-slice');
