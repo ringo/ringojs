0517a120a897c979fb8bd664e96de99b18b5be17
Hannes Wallnöfer <hannesw@gmail.com>
2009-11-18 10:33:30 +0100

    Remove date conversion function from core/number

--- modules/core/number.js
+++ modules/core/number.js
@@ -1,21 +1,12 @@
 /**
- * Time unit conversion utility methods.
- *
- * @example <pre>  (5).minutes()
- *   300000
- *
- *   (5).minutes().inSeconds()
- *   300
- *
- *   (5).minutes().ago()
- *   Fri Oct 31 2008 12:54:31 GMT+0100 (MEZ)
- *
- *   (5).minutes().fromNow()
- *   Fri Oct 31 2008 13:04:34 GMT+0100 (MEZ)</pre>
+ * @fileoverview Adds useful methods to the JS Number type.
  */
 
 module.shared = true;
 
+/**
+ * Format a number using java.text.DecimalFormat.
+ */
 Object.defineProperty(Number.prototype, 'format', {
     value: function(fmt, locale) {
         var symbols;
@@ -29,129 +20,9 @@ Object.defineProperty(Number.prototype, 'format', {
     }
 });
 
-Object.defineProperty(Number.prototype, 'millis', {
-    value: function() {
-        return this;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'seconds', {
-    value: function() {
-        return this * 1000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'minutes', {
-    value: function() {
-        return this * 60000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'hours', {
-    value: function() {
-        return this * 3600000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'days', {
-    value: function() {
-        return this * 86400000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'weeks', {
-    value: function() {
-        return this * 604800000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'months', {
-    value: function() {
-        return this * 2592000000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'years', {
-    value: function() {
-        return this * 31536000000;
-    }
-});
-
-
-Object.defineProperty(Number.prototype, 'inMillis', {
-    value: function() {
-        return this;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'inSeconds', {
-    value: function() {
-        return this / 1000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'inMinutes', {
-    value: function() {
-        return this / 60000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'inHours', {
-    value: function() {
-        return this / 3600000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'inDays', {
-    value: function() {
-        return this / 86400000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'inWeeks', {
-    value: function() {
-        return this / 604800000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'inMonths', {
-    value: function() {
-        return this / 2592000000;
-    }
-});
-
-Object.defineProperty(Number.prototype, 'inYears', {
-    value: function() {
-        return this / 31536000000;
-    }
-});
-
-
-Object.defineProperty(Number.prototype, 'ago', {
-    value: function() {
-        return new Date(Date.now() - this);
-    }
-});
-
-Object.defineProperty(Number.prototype, 'fromNow', {
-    value: function() {
-        return new Date(Date.now() + this);
-    }
-});
-
-Object.defineProperty(Number.prototype, 'before', {
-    value: function(date) {
-        return new Date(date.getTime() - this);
-    }
-});
-
-Object.defineProperty(Number.prototype, 'after', {
-    value: function(date) {
-        return new Date(date.getTime() + this);
-    }
-});
-
-
+/**
+ * Invoke a function n times with n the value of this number.
+ */
 Object.defineProperty(Number.prototype, 'times', {
     value: function(fun) {
         for (var i = 0; i < this; i++) {
