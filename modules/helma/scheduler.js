c9efd55525bf830dc15e81433f0a9be60ca94f1b
Hannes Wallnöfer <hannesw@gmail.com>
2009-12-07 15:46:40 +0100

    Add helma/scheduler module providing basic setTimeout()/setInterval() functionality

---
+++ modules/helma/scheduler.js
@@ -0,0 +1,68 @@
+/**
+ * @fileoverview This module provides support for scheduling invocation of functions.
+ */
+
+module.shared = true;
+
+var executor = executor || java.util.concurrent.Executors.newScheduledThreadPool(1);
+var MILLIS = java.util.concurrent.TimeUnit.MILLISECONDS;
+
+/**
+ * Executes a function after specified delay.
+ * @param {function} callback a function
+ * @param {number} delay the delay in milliseconds
+ * @param [args] optional arguments to pass to the function
+ * @returns {object} an id object useful for cancelling the scheduled invocation
+ */
+exports.setTimeout = function(callback, delay) {
+    var args = Array.slice(arguments, 2);
+    var runnable = new java.lang.Runnable({
+        run: function() {
+            callback.apply(global, args);
+        }
+    });
+    return executor.schedule(runnable, delay, MILLIS);
+};
+
+/**
+ * Cancel a timeout previuosly scheduled with setTimeout().
+ * @param {object} id the id object returned by setTimeout()
+ * @see setTimeout
+ */
+exports.clearTimeout = function(id) {
+    try {
+        id.cancel(false);
+    } catch (e) {
+        // ignore
+    }
+};
+
+/**
+ * Calls a function repeatedly, with a fixed time delay between each call to that function.
+ * @param {function} callback a function
+ * @param {number} delay the delay in milliseconds
+ * @param [args] optional arguments to pass to the function
+ * @returns {object} an id object useful for cancelling the scheduled invocation
+ */
+exports.setInterval = function(callback, delay) {
+    var args = Array.slice(arguments, 2);
+    var runnable = new java.lang.Runnable({
+        run: function() {
+            callback.apply(global, args);
+        }
+    });
+    return executor.scheduleAtFixedRate(runnable, 0, delay, MILLIS);
+};
+
+/**
+ * Cancel a timeout previuosly scheduled with setInterval().
+ * @param {object} id the id object returned by setInterval()
+ * @see setInterval
+ */
+exports.clearInterval = function(future) {
+    try {
+        future.cancel(false);
+    } catch (e) {
+        // ignore
+    }
+};
