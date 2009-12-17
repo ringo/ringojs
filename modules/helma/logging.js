177cf216bc0390c20453fab394562b107ebc4fa0
Hannes Wallnöfer <hannesw@gmail.com>
2009-12-17 02:19:25 +0100

    Implement logger in JS for future flexibility.

--- modules/helma/logging.js
+++ modules/helma/logging.js
@@ -1,8 +1,8 @@
 /**
- * @fileoverview This module provides generic logging support. It is optimized for
- * use with <a href="http://logging.apache.org/log4j/">Apache log4j</a> but can be
- * used with any Logging framework supported by
- * <a href="http://www.slf4j.org/">Simple Logging Facade for Java (SLF4J)</a>.
+ * @fileoverview This module provides generic logging support. It uses
+ * <a href="http://logging.apache.org/log4j/">Apache log4j</a> by default,
+ * but can be used with any Logging framework supported by
+ * <a href="http://www.slf4j.org/">SLF4J</a>.
  */
 
 require('core/string');
@@ -11,6 +11,62 @@ var LoggerFactory = org.slf4j.LoggerFactory;
 
 module.shared = true;
 var configured = false;
+
+/**
+ * Logger class. This constructor is not exported, use this module's
+ * {@link getLogger} to get a logger instance. 
+ * @param name
+ * @constructor
+ * @name Logger
+ * @see getLogger
+ */
+function Logger(name) {
+
+    var log = LoggerFactory.getLogger(name.replace(/\//g, '.'));
+
+    /**
+     * Log a debug message.
+     */
+    this.trace = function() {
+        log.trace.apply(log, arguments);
+    };
+
+    this.debug = function() {
+        log.debug.apply(log, arguments);
+    };
+
+    this.info = function() {
+        log.info.apply(log, arguments);
+    };
+
+    this.warn = function() {
+        log.warn.apply(log, arguments);
+    };
+
+    this.error = function() {
+        log.error.apply(log, arguments);
+    };
+
+    this.isTraceEnabled = function() {
+        return log.isTraceEnabled();
+    };
+
+    this.isDebugEnabled = function() {
+        return log.isDebugEnabled();
+    };
+
+    this.isInfoEnabled = function() {
+        return log.isInfoEnabled();
+    };
+
+    this.isWarnEnabled = function() {
+        return log.isWarnEnabled();
+    };
+
+    this.isErrorEnabled = function() {
+        return log.isErrorEnabled();
+    };
+}
 
 /**
  * Configure log4j using the given file resource.
@@ -33,13 +89,15 @@ var setConfig = exports.setConfig = function(resource) {
 };
 
 /**
- * Get a logger for the given name. Hang on there, we'll add documentation for the logger interface soon.
+ * Get a logger for the given name. 
+ * @param {string} name the name of the logger
+ * @returns {Logger} a logger instance for the given name
  */
 var getLogger = exports.getLogger = function(name) {
     if (!configured) {
         // getResource('foo').name gets us the absolute path to a local resource
         this.setConfig(getResource('config/log4j.properties'));
     }
-    return LoggerFactory.getLogger(name.replace(/\//g, '.'));
+    return new Logger(name);
 };
 
