0dc25835ae0f22d66b16efb00c8c7fcc4331ad0c
Robert Thurnher <r.thurnher@gmail.com>
2010-02-26 00:35:01 +0100

    Add core/string tests (initial).

--- test/all.js
+++ test/all.js
@@ -6,6 +6,7 @@ exports.object = require('./core/object_test');
 exports.file = require('./ringo/file_test');
 exports.fileutils = require('./ringo/fileutils_test');
 exports.skin = require('./ringo/skin_test');
+exports.string = require('./core/string_test');
 exports.file = require('./file/all');
 exports.binary = require('./binary/all');
 exports.repository = require('./repository/all');
