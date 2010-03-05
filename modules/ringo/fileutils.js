b0c597fbe0e1237bbf093982292b97c1a2848c69
Hannes Wallnöfer <hannesw@gmail.com>
2010-03-05 10:25:36 +0100

    Give ringo/fileutils its own version of resolve() to make sure we always use forward slashes.

--- modules/ringo/fileutils.js
+++ modules/ringo/fileutils.js
@@ -1,10 +1,58 @@
-var file = require("file");
-
 /**
  * @fileOverview A collection of file related utilities.
  */
 
-export ('resolveRelative', 'createTempFile');
+export ('resolve', 'resolveRelative', 'createTempFile');
+
+/**
+ * Resolve an arbitrary number of path elements relative to each other.
+ * This is an adapted version of the file module's resolve function that always
+ * and strictly uses forward slashes as file separators. This makes it
+ * usable for resolving module ids, resource paths, and URI paths.
+ * Originally adapted for helma/file from narwhal's file module.
+ */
+function resolve() {
+    var root = '';
+    var elements = [];
+    var leaf = '';
+    var path;
+    var SEPARATOR = '/';
+    var SEPARATOR_RE = /\//;
+    for (var i = 0; i < arguments.length; i++) {
+        path = String(arguments[i]);
+        if (path.trim() == '') {
+            continue;
+        }
+        var parts = path.split(SEPARATOR_RE);
+        if (path[0] == '/') {
+            // path is absolute, throw away everyting we have so far
+            root = parts.shift() + SEPARATOR;
+            elements = [];
+        }
+        leaf = parts.pop();
+        if (leaf == '.' || leaf == '..') {
+            parts.push(leaf);
+            leaf = '';
+        }
+        for (var j = 0; j < parts.length; j++) {
+            var part = parts[j];
+            if (part == '..') {
+                if (elements.length > 0 && elements.peek() != '..') {
+                    elements.pop();
+                } else if (!root) {
+                    elements.push(part);
+                }
+            } else if (part != '' && part != '.') {
+                elements.push(part);
+            }
+        }
+    }
+    path = elements.join(SEPARATOR);
+    if (path.length > 0) {
+        leaf = SEPARATOR + leaf;
+    }
+    return root + path + leaf;
+}
 
 /**
  * Resolve path fragment child relative to parent but only
@@ -17,7 +65,7 @@ export ('resolveRelative', 'createTempFile');
  */
 function resolveRelative(parent, child) {
     return child.startsWith(".") ?
-           file.resolve(parent, child) : child;
+           resolve(parent, child) : child;
 }
 
 /**
