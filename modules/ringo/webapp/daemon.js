8de5ffdbe75266feeae6429844831dc53c0ca855
Hannes Wallnöfer <hannesw@gmail.com>
2010-03-29 15:42:31 +0200

    Further consolidation of webapp startup scripts, add jars property for webapp configs to add stuff to classpath.

--- modules/ringo/webapp/daemon.js
+++ modules/ringo/webapp/daemon.js
@@ -3,46 +3,72 @@
  * the Apache Commons Daemon jsvc tool.
  */
 
-var system = require('system');
+var Parser = require('ringo/args').Parser;
 var Server = require('ringo/httpserver').Server;
-var server, options;
-var log = require('ringo/logging').getLogger(module.id);
 var fileutils = require('ringo/fileutils');
+var system = require('system');
+var log = require('ringo/logging').getLogger(module.id);
 
-export('init', 'start', 'stop', 'destroy', 'getServer');
+var options,
+    server,
+    parser;
 
+module.shared = true;
 
-// parse command line options
-var parser = new (require('ringo/args').Parser);
-parser.addOption("a", "app", "APP", "The exported property name of the JSGI app (default: 'app')");
-parser.addOption("c", "config", "MODULE", "The module containing the JSGI app (default: 'config')");
-parser.addOption("j", "jetty-config", "PATH", "The jetty xml configuration file (default. 'config/jetty.xml')");
-parser.addOption("H", "host", "ADDRESS", "The IP address to bind to (default: 0.0.0.0)");
-parser.addOption("m", "mountpoint", "PATH", "The URI path where to mount the application (default: /)");
-parser.addOption("p", "port", "PORT", "The TCP port to listen on (default: 80)");
-parser.addOption("s", "static-dir", "DIR", "A directory with static resources to serve");
-parser.addOption("S", "static-mountpoint", "PATH", "The URI path where ot mount the static resources");
-// parser.addOption("v", "virtual-host", "VHOST", "The virtual host name (default: undefined)");
-parser.addOption("h", "help", null, "Print help message to stdout");
+export('parseOptions', 'init', 'start', 'stop', 'destroy', 'getServer');
+
+function parseOptions(arguments, defaults) {
+    // parse command line options
+    parser = new Parser();
+    parser.addOption("a", "app", "APP", "The exported property name of the JSGI app (default: 'app')");
+    parser.addOption("c", "config", "MODULE", "The module containing the JSGI app (default: 'config')");
+    parser.addOption("j", "jetty-config", "PATH", "The jetty xml configuration file (default. 'config/jetty.xml')");
+    parser.addOption("H", "host", "ADDRESS", "The IP address to bind to (default: 0.0.0.0)");
+    parser.addOption("m", "mountpoint", "PATH", "The URI path where to mount the application (default: /)");
+    parser.addOption("p", "port", "PORT", "The TCP port to listen on (default: 80)");
+    parser.addOption("s", "static-dir", "DIR", "A directory with static resources to serve");
+    parser.addOption("S", "static-mountpoint", "PATH", "The URI path where ot mount the static resources");
+    // parser.addOption("v", "virtual-host", "VHOST", "The virtual host name (default: undefined)");
+    parser.addOption("h", "help", null, "Print help message to stdout");
+    options = parser.parse(arguments, defaults);
+    return options;
+}
 
 function init() {
     log.info("init");
-    options = {
-        config: "config",
-        app: "app",
-        port: 80
-    };
-    parser.parse(Array.slice(system.args, 1), options);
+    if (!options) {
+        parseOptions(system.args.slice(1), {
+            app: "app",
+            config: "config",
+            port: 80
+        });
+    }
     if (options.help) {
         print("Available options:");
         print(parser.help());
+    }
+
+    var config = require(options.config || "config");
+
+    // check for jar files to add to classpath
+    if (Array.isArray(config.jars)) {
+        config.jars.forEach(function(p) {
+            log.info("Adding to classpath: " + p);
+            addToClasspath(p);
+        });
+    }
+    // pick up extra http-config from config module
+    if (config.httpConfig) {
+        options = Object.merge(options, config.httpConfig);
     }
     server = new Server(options);
 }
 
 function start() {
     log.info("start");
-    var config = require(options.config);
+    var config = require(options.config || "config");
+
+    // check for additional static mounts
     if (Array.isArray(config.static)) {
         config.static.forEach(function(spec) {
             var dir = fileutils.resolveId(options.config, spec[1]);
@@ -60,6 +86,7 @@ function stop() {
 function destroy() {
     log.info("destroy");
     server.destroy();
+    server = null;
 }
 
 function getServer() {
