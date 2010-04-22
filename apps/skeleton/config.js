1d93223edceb222753d91d1d2e20e3022419e2b0
Andreas Bolka <a@bolka.at>
2010-04-22 15:37:50 +0200

    Minor stylistic fixes in webapp skeleton

--- apps/skeleton/config.js
+++ apps/skeleton/config.js
@@ -1,24 +1,24 @@
 exports.httpConfig = {
-  staticDir: 'static'
+    staticDir: 'static'
 };
 
 exports.urls = [
-    [ '/', 'actions' ]
+    ['/', 'actions'],
 ];
 
 exports.middleware = [
     'ringo/middleware/etag',
     'ringo/middleware/responselog',
     'ringo/middleware/error',
-    'ringo/middleware/notfound'
-    // 'ringo/middleware/profiler'
+    'ringo/middleware/notfound',
+    // 'ringo/middleware/profiler',
 ];
 
 exports.app = require('ringo/webapp').handleRequest;
 
 exports.macros = [
     'ringo/skin/macros',
-    'ringo/skin/filters'
+    'ringo/skin/filters',
 ];
 
 exports.charset = 'UTF-8';
