88289768f484f6ab7835e067c009344fcee1638f
Hannes Wallnöfer <hannesw@gmail.com>
2010-04-27 11:55:05 +0200

    Escape jsdoc for markdown processing.

--- modules/core/regexp.js
+++ modules/core/regexp.js
@@ -5,8 +5,8 @@ module.shared = true;
 /**
  * Accepts a string; returns the string with regex metacharacters escaped.
  * the returned string can safely be used within a regex to match a literal
- * string. escaped characters are [, ], {, }, (, ), -, *, +, ?, ., \, ^, $,
- * |, #, [comma], and whitespace.
+ * string. escaped characters are \[, ], {, }, (, ), -, *, +, ?, ., \, ^, $,
+ * |, #, \[comma], and whitespace.
  * @param {String} str the string to escape
  * @returns {String} the escaped string
  */
