20c2717b2286a550739d63aa1b7d40f22e171746
Hannes Wallnöfer <hannesw@gmail.com>
2010-05-06 11:32:35 +0200

    Use repository instead of filesystem API to make packages work with non-file resources

--- modules/packages.js
+++ modules/packages.js
@@ -5,6 +5,7 @@
 
 var system = require('system');
 var fs = require('fs');
+var engine = require('ringo/engine');
 
 module.shared = true;
 
@@ -19,30 +20,26 @@ function load() {
 
     catalog = {};
     // loop through packages and configure resources
-    var packages = fs.join(system.prefix, 'packages');
+    var packages = engine.getRingoHome().getChildRepository("packages");
 
-    if (!fs.isDirectory(packages)) {
-        print("No packages directory, skipping package setup");
+    if (!packages.exists()) {
+        print("No packages installed, skipping package setup");
         return;
     }
 
-    for each (var pkg in fs.list(packages)) {
-
-        var directory = fs.join(packages, pkg);
-        if (!fs.isDirectory(directory)) {
-            continue;
-        }
+    for each (var directory in packages.getRepositories()) {
 
         try {
-            var jsonPath = fs.join(directory, "package.json");
-            var package = JSON.parse(fs.read(jsonPath));
+            var jsonPath = directory.getResource("package.json");
+            var package = JSON.parse(jsonPath.getContent());
+            var name = package.name || directory.getName();
             package.directory = directory;
 
             // check engine specific libs
             handleResource(package.engines || "engines", function(engines) {
-                var dir = fs.join(directory, engines);
+                var dir = directory.getChildRepository("engines");
                 for each (var engine in system.engines) {
-                    var path = fs.join(dir, engine, "lib");
+                    var path = dir.getChildRepository(engine + "/lib");
                     if (fs.isDirectory(path)) {
                         require.paths.push(path);
                     }
@@ -51,24 +48,26 @@ function load() {
 
             var lib = (package.directories && package.directories.lib) || package.lib || "lib";
             handleResource(lib, function(lib) {
-                require.paths.push(fs.join(directory, lib));
+                require.paths.push(directory.getChildRepository(lib));
             });
 
             var jars = (package.directories && package.directories.jars) || package.jars;
             handleResource(jars, function(res) {
-                var path = fs.join(directory, res);
-                var files = fs.isDirectory(path)
-                    ? [fs.join(path, x) for each (x in fs.list(path)) if (x.match(/\.jar$/))]
-                    : [path]
-                ;
+                var files;
+                var path = directory.getChildRepository(res);
+                if (path.exists()) {
+                    files = [p for each (p in path.getResources()) if (p.getName().match(/\.jar$/))];
+                } else {
+                    files = [directory.getResource(res)];
+                }
                 for each (var file in files)
                     addToClasspath(file);
             });
 
-            catalog[pkg] = package;
+            catalog[name] = package;
 
         } catch (error) {
-            system.stderr.print("Error configuring package " + pkg + ": " + error);
+            system.stderr.print("Error configuring package " + directory + ": " + error);
         }
     }
 }
