861817b9ec08dcd1bbdb2a67aa1f0dce759ec501
Andreas Bolka <a@bolka.at>
2010-05-11 01:34:53 +0200

    Fix whitespace and linewidth

--- test/binary/bytestring-tests.js
+++ test/binary/bytestring-tests.js
@@ -5,7 +5,7 @@ include("binary");
 
 exports.testByteStringConstructor = function() {
     var testArray = [1,2,3,4];
-    
+
     // ByteString()
     // Construct an empty byte string.
     var b1 = new ByteString();
@@ -14,7 +14,7 @@ exports.testByteStringConstructor = function() {
     assertEqual(0, b1.length);
     b1.length = 123;
     assertEqual(0, b1.length);
-    
+
     // ByteString(byteString)
     // Copies byteString.
     var b2 = new ByteString(new ByteString(testArray));
@@ -23,7 +23,7 @@ exports.testByteStringConstructor = function() {
     assertEqual(testArray.length, b2.length);
     assertEqual(1, b2.get(0));
     assertEqual(4, b2.get(3));
-    
+
     // ByteString(byteArray)
     // Use the contents of byteArray.
     var b2 = new ByteString(new ByteArray(testArray));
@@ -32,10 +32,11 @@ exports.testByteStringConstructor = function() {
     assertEqual(testArray.length, b2.length);
     assertEqual(1, b2.get(0));
     assertEqual(4, b2.get(3));
-    
+
     // ByteString(arrayOfNumbers)
     // Use the numbers in arrayOfNumbers as the bytes.
-    // If any element is outside the range 0...255, an exception (TODO) is thrown.
+    // If any element is outside the range 0...255, an exception (TODO) is
+    // thrown.
     var b3 = new ByteString(testArray);
     assertEqual(testArray.length, b3.length);
     b3.length = 123;
@@ -50,7 +51,7 @@ exports.testByteStringConstructor = function() {
 exports.testToByteArray = function() {
     var b1 = new ByteString([1,2,3]),
         b2 = b1.toByteArray();
-        
+
     assertTrue(b2 instanceof ByteArray, "not instanceof ByteArray");
     assertEqual(b1.length, b2.length);
     assertEqual(b1.get(0), b2.get(0));
@@ -60,7 +61,7 @@ exports.testToByteArray = function() {
 exports.testToByteString = function() {
     var b1 = new ByteString([1,2,3]),
         b2 = b1.toByteString();
-        
+
     assertEqual(b1.length, b2.length);
     assertEqual(b1.get(0), b2.get(0));
     assertEqual(b1.get(2), b2.get(2));
@@ -70,40 +71,41 @@ exports.testToArray = function() {
     var testArray = [0,1,254,255],
         b1 = new ByteString(testArray),
         a1 = b1.toArray();
-          
+
     assertEqual(testArray.length, a1.length);
     for (var i = 0; i < testArray.length; i++)
         assertEqual(testArray[i], a1[i]);
 };
 
 exports.testToString = function() {
-    // the format of the resulting string isn't specified, but it shouldn't be the decoded string
+    // the format of the resulting string isn't specified, but it shouldn't be
+    // the decoded string
     // TODO: is this an ok test?
-    
+
     var testString = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+
                      "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
         testArray = [];
     for (var i = 0; i < 128; i++) testArray.push(65);
-    
+
     var resultString = new ByteString(testArray).toString();
-    
+
     assertTrue(resultString.length < 100);
     assertTrue(resultString !== testString);
 };
 
 exports.testIndexOf = function() {
     var b1 = new ByteString([0,1,2,3,4,5,0,1,2,3,4,5]);
-    
+
     assertEqual(-1, b1.indexOf(-1));
-    
+
     assertEqual(0,  b1.indexOf(0));
     assertEqual(5,  b1.indexOf(5));
     assertEqual(-1, b1.indexOf(12));
-    
+
     assertEqual(6,  b1.indexOf(0, 6));
     assertEqual(11,  b1.indexOf(5, 6));
     assertEqual(-1, b1.indexOf(12, 6));
-    
+
     assertEqual(0,  b1.indexOf(0, 0, 3));
     assertEqual(-1,  b1.indexOf(5, 0, 3));
     assertEqual(-1, b1.indexOf(12, 0, 3));
@@ -129,7 +131,7 @@ exports.testLastIndexOf = function() {
 
 exports.testCharCodeAt = function() {
     var b1 = new ByteString([0,1,2,3,4,255]);
-    
+
     assertTrue(isNaN(b1.charCodeAt(-1)));
     assertEqual(0, b1.charCodeAt(0));
     assertEqual(255, b1.charCodeAt(5));
@@ -179,14 +181,14 @@ exports.testCharAt = function() {
 
 exports.testSplit = function() {
     var b1 = new ByteString([0,1,2,3,4,5]), a1;
-    
+
     a1 = b1.split([]);
     assertEqual(1, a1.length);
     assertTrue(a1[0] instanceof ByteString);
     assertEqual(6, a1[0].length);
     assertEqual(0, a1[0].get(0));
     assertEqual(5, a1[0].get(5));
-    
+
     a1 = b1.split([2]);
     assertEqual(2, a1.length);
     assertTrue(a1[0] instanceof ByteString);
@@ -196,7 +198,7 @@ exports.testSplit = function() {
     assertEqual(3, a1[1].length);
     assertEqual(3, a1[1].get(0));
     assertEqual(5, a1[1].get(2));
-    
+
     a1 = b1.split([2], { includeDelimiter : true });
     assertEqual(3, a1.length);
     assertTrue(a1[0] instanceof ByteString);
@@ -208,7 +210,7 @@ exports.testSplit = function() {
     assertEqual(3, a1[2].length);
     assertEqual(3, a1[2].get(0));
     assertEqual(5, a1[2].get(2));
-    
+
     a1 = b1.split(new ByteString([2,3]));
     assertEqual(2, a1.length);
     assertTrue(a1[0] instanceof ByteString);
@@ -222,32 +224,32 @@ exports.testSplit = function() {
 
 exports.testSlice = function() {
     var b1 = new ByteString([0,1,2,3,4,5]), b2;
-    
+
     b2 = b1.slice();
     assertEqual(6, b2.length);
     assertEqual(0, b2.get(0));
     assertEqual(5, b2.get(5));
-    
+
     b2 = b1.slice(0);
     assertEqual(6, b2.length);
     assertEqual(0, b2.get(0));
     assertEqual(5, b2.get(5));
-    
+
     b2 = b1.slice(1, 4);
     assertEqual(3, b2.length);
     assertEqual(1, b2.get(0));
     assertEqual(3, b2.get(2));
-    
+
     b2 = b1.slice(0, -1);
     assertEqual(5, b2.length);
     assertEqual(0, b2.get(0));
     assertEqual(4, b2.get(4));
-    
+
     b2 = b1.slice(-3, -1);
     assertEqual(2, b2.length);
     assertEqual(3, b2.get(0));
     assertEqual(4, b2.get(1));
-    
+
     b2 = b1.slice(9, 10);
     assertEqual(0, b2.length);
 };
