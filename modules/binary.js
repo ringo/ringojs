60c29f0e37a8f8c7693317ce2c5394bd8729646d
Andreas Bolka <a@bolka.at>
2010-05-11 00:48:33 +0200

    Fix whitespace, linewidth & typos

--- modules/binary.js
+++ modules/binary.js
@@ -10,8 +10,8 @@
  * sequence. The ByteArray and ByteString constructors can take several
  * arguments. Have a look at the proposal for details.</p>
  *
- * <p>When passed to a Java method that expects a byte array, instances of thes
- * class are automatically unwrapped. Use the {@link #unwrap()} method to
+ * <p>When passed to a Java method that expects a byte array, instances of
+ * these class are automatically unwrapped. Use the {@link #unwrap()} method to
  * explicitly get the wrapped Java byte array.</p>
  */
 
@@ -27,10 +27,10 @@ exports.Binary = Binary;
 
 /**
  * Constructs a writable and growable byte array.
- * @param {Binary|Array|String} content the content of the ByteArray. If the first argument
- *        is a String, the constructor requires a second argument containing the
- *        name of the String's encoding. If called without arguments, an empty ByteString
- *        is returned.
+ * @param {Binary|Array|String} content the content of the ByteArray. If the
+ *        first argument is a String, the constructor requires a second
+ *        argument containing the name of the String's encoding. If called
+ *        without arguments, an empty ByteString is returned.
  * @param {String} charset the encoding name if the first argument is a String.
  * @constructor
  */
@@ -38,10 +38,10 @@ exports.ByteArray = ByteArray;
 
 /**
  * Constructs an immutable byte string.
- * @param {Binary|Array|String} content the content of the ByteString. If the first argument
- *        is a String, the constructor requires a second argument containing the
- *        name of the String's encoding. If called without arguments, an empty ByteString
- *        is returned.
+ * @param {Binary|Array|String} content the content of the ByteString. If the
+ *        first argument is a String, the constructor requires a second
+ *        argument containing the name of the String's encoding. If called
+ *        without arguments, an empty ByteString is returned.
  * @param {String} charset the encoding name if the first argument is a String.
  * @constructor
  */
@@ -60,7 +60,7 @@ Object.defineProperty(String.prototype, 'toByteArray', {
 });
 
 /**
- * Converts the String to an immutable ByteSTring using the specified encoding.
+ * Converts the String to an immutable ByteString using the specified encoding.
  * @param {String} charset the name of the string encoding. Defaults to 'UTF-8'
  * @returns a ByteArray representing the string
  */
@@ -118,7 +118,8 @@ Object.defineProperty(ByteArray.prototype, 'filter', {
 });
 
 /**
- * Tests whether some element in the array passes the test implemented by the provided function.
+ * Tests whether some element in the array passes the test implemented by the
+ * provided function.
  * @param {Function} callback the callback function
  * @param {Object} thisObj optional this-object for callback
  * @returns {Boolean} true if at least one invocation of callback returns true
@@ -130,7 +131,8 @@ Object.defineProperty(ByteArray.prototype, 'some', {
 });
 
 /**
- * Tests whether all elements in the array pass the test implemented by the provided function.
+ * Tests whether all elements in the array pass the test implemented by the
+ * provided function.
  * @param {Function} callback the callback function
  * @param {Object} thisObj optional this-object for callback
  * @returns {Boolean} true if every invocation of callback returns true
@@ -142,8 +144,8 @@ Object.defineProperty(ByteArray.prototype, 'every', {
 });
 
 /**
- * Returns a new ByteArray whose content is the result of calling the provided function
- * with every element of the original ByteArray
+ * Returns a new ByteArray whose content is the result of calling the provided
+ * function with every element of the original ByteArray
  * @param {Function} callback the callback
  * @param {Object} thisObj optional this-object for callback
  * @returns {ByteArray} a new ByteArray
@@ -155,9 +157,12 @@ Object.defineProperty(ByteArray.prototype, 'map', {
 });
 
 /**
- * Apply a function to each element in this ByteArray as to reduce its content to a single value.
- * @param {Function} callback the function to call with each element of the ByteArray
- * @param initialValue optional argument to be used as the first argument to the first call to the callback
+ * Apply a function to each element in this ByteArray as to reduce its content
+ * to a single value.
+ * @param {Function} callback the function to call with each element of the
+ *        ByteArray
+ * @param initialValue optional argument to be used as the first argument to
+ *        the first call to the callback
  * @returns the return value of the last callback invocation
  * @see https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Global_Objects/Array/reduce
  */
@@ -170,10 +175,12 @@ Object.defineProperty(ByteArray.prototype, 'reduce', {
 });
 
 /**
- * Apply a function to each element in this ByteArray starting at the last element as to reduce
- * its content to a single value.
- * @param {Function} callback the function to call with each element of the ByteArray
- * @param initialValue optional argument to be used as the first argument to the first call to the callback
+ * Apply a function to each element in this ByteArray starting at the last
+ * element as to reduce its content to a single value.
+ * @param {Function} callback the function to call with each element of the
+ *        ByteArray
+ * @param initialValue optional argument to be used as the first argument to
+ *        the first call to the callback
  * @returns the return value of the last callback invocation
  * @see ByteArray.prototype.reduce
  * @see https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Global_Objects/Array/reduceRight
@@ -209,7 +216,7 @@ Object.defineProperty(ByteArray.prototype, 'push', {
 
 /**
  * Removes the first element from the ByteArray and returns that element.
- * This method changes the length of the ByteArray 
+ * This method changes the length of the ByteArray
  * @returns {Number} the removed first element
  */
 Object.defineProperty(ByteArray.prototype, 'shift', {
@@ -219,7 +226,8 @@ Object.defineProperty(ByteArray.prototype, 'shift', {
 });
 
 /**
- * Adds one or more elements to the beginning of the ByteArray and returns its new length.
+ * Adds one or more elements to the beginning of the ByteArray and returns its
+ * new length.
  * @param {Number} num... one or more numbers to append
  * @returns {Number} the new length of the ByteArray
  */
@@ -230,9 +238,11 @@ Object.defineProperty(ByteArray.prototype, 'unshift', {
 });
 
 /**
- * Changes the content of the ByteArray, adding new elements while removing old elements.
+ * Changes the content of the ByteArray, adding new elements while removing old
+ * elements.
  * @param {Number} index the index at which to start changing the ByteArray
- * @param {Number} howMany The number of elements to remove at the given position
+ * @param {Number} howMany The number of elements to remove at the given
+ *        position
  * @param {Number} elements... the new elements to add at the given position
  */
 Object.defineProperty(ByteArray.prototype, 'splice', {
@@ -242,7 +252,8 @@ Object.defineProperty(ByteArray.prototype, 'splice', {
 });
 
 /**
- * Copy a range of bytes between start and stop from this object to another ByteArray at the given target offset.
+ * Copy a range of bytes between start and stop from this object to another
+ * ByteArray at the given target offset.
  * @param {Number} start
  * @param {Number} end
  * @param {ByteArray} target
@@ -253,8 +264,8 @@ Object.defineProperty(ByteArray.prototype, 'splice', {
 
 /**
  * The length in bytes. This property is writable. Setting it to a value higher
- * than the current value fills the new slots with 0, setting it to a lower value
- * truncates the byte array.
+ * than the current value fills the new slots with 0, setting it to a lower
+ * value truncates the byte array.
  * @type Number
  * @name ByteArray.prototype.length
  */
@@ -262,11 +273,13 @@ Object.defineProperty(ByteArray.prototype, 'splice', {
 /**
  * Returns a new ByteArray containing a portion of this ByteArray.
  * @param {Number} begin Zero-based index at which to begin extraction.
- *       As a negative index, begin indicates an offset from the end of the sequence.
+ *        As a negative index, begin indicates an offset from the end of the
+ *        sequence.
  * @param {Number} end  Zero-based index at which to end extraction.
- *       slice extracts up to but not including end.
- *       As a negative index, end indicates an offset from the end of the sequence.
- *       If end is omitted, slice extracts to the end of the sequence.
+ *        slice extracts up to but not including end.
+ *        As a negative index, end indicates an offset from the end of the
+ *        sequence.
+ *        If end is omitted, slice extracts to the end of the sequence.
  * @returns {ByteArray} a new ByteArray
  * @name ByteArray.prototype.slice
  * @function
@@ -276,7 +289,7 @@ Object.defineProperty(ByteArray.prototype, 'splice', {
  * Returns a ByteArray composed of itself concatenated with the given
  * ByteString, ByteArray, and Array values.
  * @param {Binary|Array} arg... one or more elements to concatenate
- * @returns {ByteArray} a new ByteArray 
+ * @returns {ByteArray} a new ByteArray
  * @name ByteArray.prototype.concat
  * @function
  */
@@ -311,9 +324,10 @@ Object.defineProperty(ByteArray.prototype, 'splice', {
  */
 
 /**
- * Returns the index of the first occurance of sequnce (a Number or a ByteString or ByteArray of any length)
- * or -1 if none was found. If start and/or stop are specified, only elements between the the indexes start
- * and stop are searched.
+ * Returns the index of the first occurrence of sequence (a Number or a
+ * ByteString or ByteArray of any length) or -1 if none was found. If start
+ * and/or stop are specified, only elements between the indexes start and stop
+ * are searched.
  * @param {Number|Binary} sequence the number or binary to look for
  * @param {Number} start optional index position at which to start searching
  * @param {Number} stop optional index position at which to stop searching
@@ -323,9 +337,10 @@ Object.defineProperty(ByteArray.prototype, 'splice', {
  */
 
 /**
- * Returns the index of the last occurance of sequnce (a Number or a ByteString or ByteArray of any length)
- * or -1 if none was found. If start and/or stop are specified, only elements between the the indexes start
- * and stop are searched.
+ * Returns the index of the last occurrence of sequence (a Number or a
+ * ByteString or ByteArray of any length) or -1 if none was found. If start
+ * and/or stop are specified, only elements between the indexes start and stop
+ * are searched.
  * @param {Number|Binary} sequence the number or binary to look for
  * @param {Number} start optional index position at which to start searching
  * @param {Number} stop optional index position at which to stop searching
@@ -335,26 +350,27 @@ Object.defineProperty(ByteArray.prototype, 'splice', {
  */
 
 /**
- * Split at delimiter, which can by a Number, a ByteString, a ByteArray or an Array of the prior
- * (containing multiple delimiters, i.e., "split at any of these delimiters").
- * Delimiters can have arbitrary size.
+ * Split at delimiter, which can by a Number, a ByteString, a ByteArray or an
+ * Array of the prior (containing multiple delimiters, i.e., "split at any of
+ * these delimiters"). Delimiters can have arbitrary size.
  * @param {Number|Binary} delimiter one or more delimiter items
- * @param {Object} options optional object parameter with the following optional properties:
- * <ul><li>count - Maximum number of elements (ignoring delimiters) to return.
- * The last returned element may contain delimiters.</li>
- * <li>includeDelimiter - Whether the delimiter should be included in the result.</li></ul>  
+ * @param {Object} options optional object parameter with the following
+ *        optional properties: <ul>
+ *        <li>count - Maximum number of elements (ignoring delimiters) to
+ *        return. The last returned element may contain delimiters.</li>
+ *        <li>includeDelimiter - Whether the delimiter should be included in
+ *        the result.</li></ul>
  * @name ByteArray.prototype.split
  * @function
  */
 
 /**
- * Create a ByteArray wrapper for a Java byte array without creating a new copy as the
- * ByteArray constructor does. Any changes made on the ByteArray instance will be applied
- * to the original byte array.
+ * Create a ByteArray wrapper for a Java byte array without creating a new copy
+ * as the ByteArray constructor does. Any changes made on the ByteArray
+ * instance will be applied to the original byte array.
  * @name ByteArray.wrap
  * @param {Binary} bytes a Java byte array or Binary instance
  * @returns {ByteArray} a ByteArray wrapping the argument
  * @function
  * @since 0.5
  */
-
