5748200774e314cf9d28b5689d65a730a707e499
Hannes Wallnöfer <hannesw@gmail.com>
2010-05-17 23:33:40 +0200

    Some tweaks in basicauth middleware, add api docs

--- modules/ringo/middleware/basicauth.js
+++ modules/ringo/middleware/basicauth.js
@@ -1,3 +1,15 @@
+/**
+ * @fileOverview Basic Authentication middleware. To apply authentication to parts of your
+ * website add something like this to your `config.js` with the long string being a SHA1
+ * digest of the user's password:
+ *
+ *     exports.auth = {
+ *         '/protected/path': {
+ *             admin: "30b93f320076de1304c34673f9f524f7ea7db709"
+ *         }
+ *     };
+ */
+
 require('core/string');
 var base64 = require('ringo/base64');
 var {auth} = require('config');
@@ -6,14 +18,20 @@ module.shared = true;
 
 exports.middleware = function (app) {
     return function (req) {
-        if (auth[req.pathInfo.replace(/^\//, '').replace(/\/$/, '')]) {
-            var toAuth = // Determine path to authorize with credentials.
-                    auth[req.pathInfo.replace(/^\//, '').replace(/\/$/, '')];
+        // normalize multiple slashes in request path
+        var path = (req.scriptName + req.pathInfo).replace(/\/+/g, '/');
+        var toAuth;
+        for (var realm in auth) {
+            if (path.indexOf(realm) == 0) {
+                toAuth = auth[realm];
+                break;
+            }
+        }
+        if (toAuth) {
             if (req.headers.authorization) { // Extract credentials from HTTP.
-                var credentials = base64.decode(req.headers.authorization.
-                        replace(/Basic /, '')).split(':');
-                if (credentials[1].digest('sha1') ===
-                        toAuth[credentials[0]]) {
+                var credentials = base64.decode(req.headers.authorization
+                        .replace(/Basic /, '')).split(':');
+                if (credentials[1].digest('sha1') === toAuth[credentials[0]]) {
                     return app(req); // Authorization.
                 }
             }
