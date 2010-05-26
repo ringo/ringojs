ringo/ringojs

commit b54c047361ae5bd015f64d7b6142edd776dda7ac

Evaluate nested macros in advance so they're always available.

modules/ringo/skin.js
@@ -61,7 +61,7 @@ function createSkin(resourceOrString) {
         return skincache[resourceOrString];
     }
     if (log.isDebugEnabled())
-        log.debug("creating skin: " + resourceOrString);
+        log.debug("creating skin: ", resourceOrString);
     var mainSkin = [];
     var subSkins = {};
     var currentSkin = mainSkin;
@@ -196,8 +196,23 @@ function Skin(mainSkin, subSkins, parentSkin, resourceOrString) {
     }
 
     function evaluateExpression(macro, context, suffix, value) {
-        if (log.isDebugEnabled())
-            log.debug('evaluating expression: ' + macro);
+        if (log.isDebugEnabled()) {
+            log.debug('evaluating expression: ', macro);
+        }
+        // evaluate nested macros
+        var paramLength = macro.parameters.length;
+        for (var p = 0; p < paramLength; p++) {
+            var param = macro.parameters[p];
+            if (param instanceof MacroTag) {
+                macro.parameters[p] = evaluateExpression(param, context, '_macro');
+            }
+        }
+        for (var k in macro.namedParameters) {
+            param = macro.namedParameters[k];
+            if (param instanceof MacroTag) {
+                macro.namedParameters[k] = evaluateExpression(param, context, '_macro');
+            }
+        }
         if (builtin[macro.name]) {
             return builtin[macro.name](macro, context);
         }
@@ -238,23 +253,19 @@ function Skin(mainSkin, subSkins, parentSkin, resourceOrString) {
     // builtin macro handlers
     var builtin = {
         "render": function(macro, context) {
-            var skinName = macro.getParameter(0);
-            var skin = getEvaluatedParameter(skinName, context, 'render:skin');
+            var skin = macro.getParameter(0);
             return skin == null ? "" : self.renderSubskin(skin, context);
         },
 
         "echo": function(macro, context) {
-            var result = macro.parameters.map(function(elem) {
-                return getEvaluatedParameter(elem, context, 'echo');
-            });
+            var result = macro.parameters;
             var wrapper = macro.getParameter("wrap") || macro.getParameter("echo-wrap");
             if (wrapper != null) {
-                wrapper = getEvaluatedParameter(wrapper, context);
                 result = result.map(function(part) {return wrapper[0] + part + wrapper[1]});
             }
             var separator = macro.getParameter("separator");
             if (separator != null) {
-                return result.join(getEvaluatedParameter(separator), context, 'for:separator');
+                return result.join(separator);
             }
             return result.join(' ');
         },
@@ -264,8 +275,8 @@ function Skin(mainSkin, subSkins, parentSkin, resourceOrString) {
                 return "[Error in for-in macro: not enough parameters]";
             if (macro.parameters[1] != "in")
                 return "[Error in for-in macro: expected in]";
-            var name = getEvaluatedParameter(macro.parameters[0], context, 'for:name');
-            var list = getEvaluatedParameter(macro.parameters[2], context, 'for:list');
+            var name = macro.parameters[0];
+            var list = macro.parameters[2];
             var subContext = context.clone();
             var subMacro = macro.getSubMacro(3);
             if (subMacro.name == "and") {
@@ -274,17 +285,16 @@ function Skin(mainSkin, subSkins, parentSkin, resourceOrString) {
             var result = [];
             for (var [index, value] in list) {
                 subContext['index'] = index
-                subContext[name] = getEvaluatedParameter(value, context, 'for:value');
+                subContext[name] = value;
                 result.push(evaluateMacro(subMacro, subContext));
             }
             var wrapper = macro.getParameter("wrap") || macro.getParameter(name + "-wrap");
             if (wrapper != null) {
-                wrapper = getEvaluatedParameter(wrapper, context);
                 result = result.map(function(part) {return wrapper[0] + part + wrapper[1]});
             }
             var separator = macro.getParameter("separator");
             if (separator != null) {
-                return result.join(getEvaluatedParameter(separator), context, 'for:separator');
+                return result.join(separator);
             }
             return result.join('');
         },
@@ -300,7 +310,7 @@ function Skin(mainSkin, subSkins, parentSkin, resourceOrString) {
             if (bypass) {
                 index++;
             } else {
-                var condition = getEvaluatedParameter(macro.parameters[index++], context, 'if:condition');
+                var condition = macro.parameters[index++];
                 result = negated ? !condition : !!condition;
             }
             var subName = macro.parameters[index];
@@ -318,27 +328,16 @@ function Skin(mainSkin, subSkins, parentSkin, resourceOrString) {
         "set": function(macro, context) {
             if (macro.parameters.length < 2)
                 return "[Error in set macro: not enough parameters]";
-            var map = getEvaluatedParameter(macro.parameters[0], context, 'with:map');
+            var map = macro.parameters[0];
             var subContext = context.clone();
             var subMacro = macro.getSubMacro(1);
             for (var [key, value] in map) {
-                subContext[key] = getEvaluatedParameter(value, context, 'with:value');;
+                subContext[key] = value;
             }
             return evaluateMacro(subMacro, subContext);
         }
 
     };
-
-    function getEvaluatedParameter(value, context, logprefix) {
-        if (log.isDebugEnabled())
-            log.debug(logprefix + ': macro called with value: ' + value);
-        if (value instanceof MacroTag) {
-            value = evaluateExpression(value, context, '_macro');
-            if (log.isDebugEnabled())
-                log.debug(logprefix + ': evaluated value macro, got ' + value);
-        }
-        return value;
-    }
 
     this.toString = function toString() {
         return "[Skin Object]";

src/org/ringojs/template/MacroTag.java
@@ -46,7 +46,7 @@ public class MacroTag extends ScriptableObject {
     static final Object[] ownIds = new Object[] {"name", "parameters",
                                                  "parameterNames", "filter"};
 
-    Object jsParams, jsNames;
+    Object jsParams, jsNames, jsNamedParams;
 
     public MacroTag() {}
 
@@ -123,6 +123,17 @@ public class MacroTag extends ScriptableObject {
             jsParams = new ScriptableList(getTopLevelScope(this), args);
         }
         return jsParams;
+    }
+
+    /**
+     * A Javascript object containing all named parameters in this macro tagg.
+     */
+    @JSGetter
+    public Object getNamedParameters() {
+        if (jsNamedParams == null) {
+            jsNamedParams = new ScriptableMap(getTopLevelScope(this), namedArgs);
+        }
+        return jsNamedParams;
     }
 
     /**

src/org/ringojs/wrappers/ScriptableMap.java
@@ -29,7 +29,7 @@ import java.util.HashMap;
 public class ScriptableMap extends NativeJavaObject {
 
     boolean reflect;
-    Map<Object,Object> map;
+    Map map;
     final static String CLASSNAME = "ScriptableMap";
 
     // Set up a custom constructor, for this class is somewhere between a host class and
@@ -38,7 +38,6 @@ public class ScriptableMap extends NativeJavaObject {
         BaseFunction ctor = new BaseFunction(scope, ScriptableObject.getFunctionPrototype(scope)) {
             @Override
             public Scriptable construct(Context cx, Scriptable scope, Object[] args) {
-                Object arg;
                 boolean reflect = false;
                 if (args.length > 2) {
                     throw new EvaluatorException("ScriptableMap() called with too many arguments");
@@ -65,9 +64,9 @@ public class ScriptableMap extends NativeJavaObject {
         if (obj instanceof Map) {
             this.map = (Map) obj;
         } else if (obj == null || obj == Undefined.instance) {
-            this.map = new HashMap<Object, Object>();
+            this.map = new HashMap();
         } else if (obj instanceof Scriptable) {
-            this.map = new HashMap<Object, Object>();
+            this.map = new HashMap();
             Scriptable s = (Scriptable) obj;
             Object[] ids = s.getIds();
             for (Object id: ids) {
@@ -87,7 +86,7 @@ public class ScriptableMap extends NativeJavaObject {
 
     }
 
-    public ScriptableMap(Scriptable scope, Map<Object, Object> map) {
+    public ScriptableMap(Scriptable scope, Map map) {
         super(scope, map, map.getClass());
         this.map = map;
         initPrototype(scope);

commit c2f82f4b3c3708fc80ab17a67cfb9d45a4e343f0

Fix nested macro expansion, add regression tests
This fixes a rather nasty bug introduced by b54c047 where nested macros
(which includes those used in the builtins <%if%> or <%for%>) were
expanded in-place, modifying the skin object itself and thus ignoring a
different context in multiple render invocations.

The "testNestedMacro" testcase illustrates what was previously going
wrong. Assuming you have a simple skin using nested macros:

    var skin = createSkin('<% echo <% foo %> %>');

The first invocation would work as expected:

    render(skin, {foo: 'a'})
    // == 'a'

Unfortunatly, this previously also "fixed" the value of `foo` in the
skin object, so future invocations would lead to unexpected results:

    render(skin, {foo: 'b'})
    // == 'a' // expected: 'b'

modules/ringo/skin.js
@@ -199,17 +199,24 @@ function Skin(mainSkin, subSkins, parentSkin, resourceOrString) {
         if (log.isDebugEnabled()) {
             log.debug('evaluating expression: ', macro);
         }
-        // evaluate nested macros
-        var paramLength = macro.parameters.length;
-        for (var p = 0; p < paramLength; p++) {
-            var param = macro.parameters[p];
+
+        // Evaluate nested macros.
+        //
+        // We need to keep the original nested "MacroTag" objects obtained from
+        // parsing around, in order to be able to safely re-evaluate the same
+        // skin in differing contexts.
+        macro.nested = macro.nested || {};
+        for (var i in macro.parameters) {
+            var param = macro.nested[i] || macro.parameters[i];
             if (param instanceof MacroTag) {
-                macro.parameters[p] = evaluateExpression(param, context, '_macro');
+                macro.nested[i] = param;
+                macro.parameters[i] = evaluateExpression(param, context, '_macro');
             }
         }
         for (var k in macro.namedParameters) {
-            param = macro.namedParameters[k];
+            var param = macro.nested[k] || macro.namedParameters[k];
             if (param instanceof MacroTag) {
+                macro.nested[k] = param;
                 macro.namedParameters[k] = evaluateExpression(param, context, '_macro');
             }
         }

test/ringo/skin_test.js
@@ -20,6 +20,16 @@ exports.testMacro = function () {
     assertEqual('before HERE after', render(skin, context));
 };
 
+exports.testNestedMacro = function () {
+    var skin = createSkin('<% echo <% foo %> %>');
+
+    assertEqual('a', render(skin, {foo: 'a'}));
+
+    // ensure that rendering a skin (with nested macros) multiple times in
+    // differing contexts works as expected.
+    assertEqual('b', render(skin, {foo: 'b'}));
+};
+
 exports.testFilter = function () {
     var skin = createSkin('before <% x | filter %> after');
     var context = {filter_filter: function (str) 'HERE'};
@@ -65,6 +75,24 @@ exports.testExternalSubskin = function () {
 
     skin = createSkin(getResource('./skins/foo.html'));
     assertEqual('foo\nbar\nbaz\n', render(skin));
+};
+
+// --- builtins ---
+
+exports.testIfBuiltin = function () {
+    var skin = createSkin('<% if <% x %> render tt %>' +
+                          '<% if not <% x %> render ff %>' +
+                          '\n<% x %>' +
+                          '<% subskin tt %>tt' +
+                          '<% subskin ff %>ff');
+
+    assertEqual('tt\ntrue',     render(skin, {x: true}).trim());
+    assertEqual('tt\n42',       render(skin, {x: 42}).trim());
+    assertEqual('tt',           render(skin, {x: []}).trim());
+
+    assertEqual('ff\nfalse',    render(skin, {x: false}).trim());
+    assertEqual('ff',           render(skin, {x: null}).trim());
+    assertEqual('ff',           render(skin, {}).trim());
 };
 
 // --- filters ---