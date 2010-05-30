7a61ad54c8015c7488b7a14136fedfedbafe5b1f
Andreas Bolka <a@bolka.at>
2010-05-30 16:55:34 +0200

    Factor loading of a single package into function

--- modules/packages.js
+++ modules/packages.js
@@ -6,7 +6,7 @@
 var system = require('system');
 var engine = require('ringo/engine');
 
-export('load', 'normalize', 'catalog');
+export('load', 'loadPackage', 'normalize', 'catalog');
 
 var catalog;
 
@@ -17,56 +17,56 @@ function load() {
     }
 
     catalog = {};
+
     // loop through packages and configure resources
     var packages = engine.getRingoHome().getChildRepository("packages");
-
     if (!packages.exists()) {
         print("No packages installed, skipping package setup");
         return;
     }
+    packages.getRepositories().forEach(loadPackage);
+}
 
-    for each (var directory in packages.getRepositories()) {
+function loadPackage(directory) {
+    try {
+        var jsonPath = directory.getResource("package.json");
+        var package = JSON.parse(jsonPath.getContent());
+        var name = package.name || directory.getName();
+        package.directory = directory;
 
-        try {
-            var jsonPath = directory.getResource("package.json");
-            var package = JSON.parse(jsonPath.getContent());
-            var name = package.name || directory.getName();
-            package.directory = directory;
-
-            // check engine specific libs
-            handleResource(package.engines || "engines", function(engines) {
-                var dir = directory.getChildRepository("engines");
-                for each (var engine in system.engines) {
-                    var path = dir.getChildRepository(engine + "/lib");
-                    if (path.exists()) {
-                        require.paths.push(path);
-                    }
-                }
-            });
-
-            var lib = (package.directories && package.directories.lib) || package.lib || "lib";
-            handleResource(lib, function(lib) {
-                require.paths.push(directory.getChildRepository(lib));
-            });
-
-            var jars = (package.directories && package.directories.jars) || package.jars;
-            handleResource(jars, function(res) {
-                var files;
-                var path = directory.getChildRepository(res);
+        // check engine specific libs
+        handleResource(package.engines || "engines", function(engines) {
+            var dir = directory.getChildRepository("engines");
+            for each (var engine in system.engines) {
+                var path = dir.getChildRepository(engine + "/lib");
                 if (path.exists()) {
-                    files = [p for each (p in path.getResources()) if (p.getName().match(/\.jar$/))];
-                } else {
-                    files = [directory.getResource(res)];
+                    require.paths.push(path);
                 }
-                for each (var file in files)
-                    addToClasspath(file);
-            });
+            }
+        });
 
-            catalog[name] = package;
+        var lib = (package.directories && package.directories.lib) || package.lib || "lib";
+        handleResource(lib, function(lib) {
+            require.paths.push(directory.getChildRepository(lib));
+        });
 
-        } catch (error) {
-            system.stderr.print("Error configuring package " + directory + ": " + error);
-        }
+        var jars = (package.directories && package.directories.jars) || package.jars;
+        handleResource(jars, function(res) {
+            var files;
+            var path = directory.getChildRepository(res);
+            if (path.exists()) {
+                files = [p for each (p in path.getResources()) if (p.getName().match(/\.jar$/))];
+            } else {
+                files = [directory.getResource(res)];
+            }
+            for each (var file in files)
+                addToClasspath(file);
+        });
+
+        catalog[name] = package;
+
+    } catch (error) {
+        system.stderr.print("Error configuring package " + directory + ": " + error);
     }
 }
 
