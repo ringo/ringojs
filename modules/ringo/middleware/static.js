7c1a5e5f484a7b8dcbd91b921a185866a7387210
tschaub <tim.schaub@gmail.com>
2010-10-28 10:24:31 -0600

    Add configuration options to static middleware to allow serving of a index file for directory paths.

--- modules/ringo/middleware/static.js
+++ modules/ringo/middleware/static.js
@@ -7,16 +7,29 @@ var {mimeType} = require("ringo/webapp/mime");
 
 /**
  * Middleware for serving static resources.
+ *
+ * #### Configuration options
+ *
+ *  The `options` object may contain the following properties:
+ *
+ *  - `index`: the name of a file to serve if the path matches a directory (e.g.
+ *    "index.html")
+ *
  * @param resourceBase the base resource directory
+ * @param options optional configuration properties
  */
-exports.middleware = function(resourceBase) {
+exports.middleware = function(resourceBase, options) {
     if (typeof resourceBase == "string") {
         resourceBase = getRepository(resourceBase);
     }
     resourceBase.setRoot();
+    var index = options && options.index;
     return function(app) {
         return function(request) {
             var path = request.pathInfo;
+            if (index && path.charAt(path.length-1) === "/") {
+                path += index;
+            }
             if (path.length > 1) {
                 var resource = resourceBase.getResource(path);
                 if (resource && resource.exists()) {
