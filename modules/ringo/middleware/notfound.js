01b319197ee370bc50695a0e9e466ad82bb6087c
tschaub <tim.schaub@gmail.com>
2010-11-02 16:12:25 -0600

    Restructure error and notfound middleware for documentation.

--- modules/ringo/middleware/notfound.js
+++ modules/ringo/middleware/notfound.js
@@ -4,31 +4,41 @@
 var {Response} = require('ringo/webapp/response');
 
 /**
- * Middleware for simple 404-Not-Found pages.
+ * Create middleware for simple 404-Not-Found pages.
+ * @example
+ * <pre>var app = middleware("templates/404.html")(app);</pre>
+ * @param {String} skin the path to a template for rendering 404 pages (defaults to 'notfound.html')
+ * @returns {Function} a function that can be used to wrap a JSGI app
  */
-exports.middleware = function(app, skin) {
-    if (arguments.length == 1) {
-        if (typeof app === 'string') {
-            skin = app;
-            return function(app) {
-                return exports.middleware(app, skin);
-            };
-        } else {
-            skin = module.resolve('notfound.html');
-        }
+exports.middleware = function(skin) {
+    var app; // backwards compatibility
+    if (arguments.length === 2) {
+        // old (app, skin) form
+        app = skin;
+        skin = arguments[1];
+    } else if (typeof skin === 'function') {
+        // old (app) form
+        app = skin;
+        skin = undefined;
     }
-    return function(request) {
-        try {
-            return app(request);
-        } catch (e if e.notfound) {
-            var res = Response.skin(skin, {
-                title: "Not Found",
-                body: "<p>The requested URL <b>" + request.scriptName
-                        + request.pathInfo + "</b> was not found on the server.</p>"
-            });
-            res.status = 404;
-            res.contentType = 'text/html';
-            return res;
-        }
-    };
+    if (!skin) {
+        skin = module.resolve('notfound.html');
+    }
+    function wrap(app) {
+        return function(request) {
+            try {
+                return app(request);
+            } catch (e if e.notfound) {
+                var res = Response.skin(skin, {
+                    title: "Not Found",
+                    body: "<p>The requested URL <b>" + request.scriptName
+                            + request.pathInfo + "</b> was not found on the server.</p>"
+                });
+                res.status = 404;
+                res.contentType = 'text/html';
+                return res;
+            }
+        };
+    }
+    return app ? wrap(app) : wrap;
 };
