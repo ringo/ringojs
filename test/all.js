4bba8a1713f0923012723539758da9cbcc8e36ae
Hannes Wallnöfer <hannesw@gmail.com>
2010-12-09 11:01:54 +0100

    Reverse inclusion logic between test/all.js and test/most.js to keep httpclient tests from overwriting logging config in demo app

--- test/all.js
+++ test/all.js
@@ -1,27 +1,14 @@
-exports.testAssert = require('./assert');
-exports.testAssertCommonJs = require('./assert_commonjs');
-exports.testBase64 = require('./ringo/base64_test');
-exports.testArgs = require('./ringo/args_test');
-exports.testBuffer = require('./ringo/buffer_test');
-exports.testEncoding = require('./ringo/encoding_test');
-exports.testEvents = require('./ringo/events_test');
-exports.testSkin = require('./ringo/skin_test');
+// Run complete RingoJS test suite.
+
+// Unless your tests take a long time to run, consume lots of resources,
+// or mess with global settings you probably should add them to ./most.js
+
+var most = require('./most');
+for (var key in most) {
+    exports[key] = most[key];
+}
+
 exports.testHttpclient = require('./ringo/httpclient_test');
-exports.testArrays = require('./ringo/utils/arrays_test');
-exports.testFiles = require('./ringo/utils/files_test');
-exports.testObjects = require('./ringo/utils/objects_test');
-exports.testRegexp = require('./ringo/utils/regexp_test');
-exports.testStrings = require('./ringo/utils/strings_test');
-exports.testDates = require('./ringo/utils/dates_test');
-exports.testHttp = require('./ringo/utils/http_test');
-exports.testFilestore = require('./ringo/storage/filestore_test');
-exports.testMemstore = require('./ringo/storage/memstore_test');
-exports.testFile = require('./file/all');
-exports.testBinary = require('./binary/all');
-exports.testRepository = require('./repository/all');
-exports.testIo = require('./io_test');
-exports.testModules = require('./modules/all');
-exports.testMiddleware = require('./ringo/middleware/all');
 
 // start the test runner if we're called directly from command line
 if (require.main == module.id) {
--- test/most.js
+++ test/most.js
@@ -1,11 +1,29 @@
-// run all tests except for the rather slow httpclient ones
+// Run most tests except for those that take a long time to run,
+// consume lots of resources, or mess with global settings
 
-var all = require('./all');
-
-for (var key in all) {
-    if (key !== "testHttpclient")
-        exports[key] = all[key];
-}
+exports.testAssert         = require('./assert');
+exports.testAssertCommonJs = require('./assert_commonjs');
+exports.testBase64         = require('./ringo/base64_test');
+exports.testArgs           = require('./ringo/args_test');
+exports.testBuffer         = require('./ringo/buffer_test');
+exports.testEncoding       = require('./ringo/encoding_test');
+exports.testEvents         = require('./ringo/events_test');
+exports.testSkin           = require('./ringo/skin_test');
+exports.testArrays         = require('./ringo/utils/arrays_test');
+exports.testFiles          = require('./ringo/utils/files_test');
+exports.testObjects        = require('./ringo/utils/objects_test');
+exports.testRegexp         = require('./ringo/utils/regexp_test');
+exports.testStrings        = require('./ringo/utils/strings_test');
+exports.testDates          = require('./ringo/utils/dates_test');
+exports.testHttp           = require('./ringo/utils/http_test');
+exports.testFilestore      = require('./ringo/storage/filestore_test');
+exports.testMemstore       = require('./ringo/storage/memstore_test');
+exports.testFile           = require('./file/all');
+exports.testBinary         = require('./binary/all');
+exports.testRepository     = require('./repository/all');
+exports.testIo             = require('./io_test');
+exports.testModules        = require('./modules/all');
+exports.testMiddleware     = require('./ringo/middleware/all');
 
 // start the test runner if we're called directly from command line
 if (require.main == module.id) {
