4be4890e4e08680a98dc7ebbd8721a470a18fd3e
Hannes Wallnöfer <hannesw@gmail.com>
2010-12-13 16:24:44 +0100

    Make sure promises test assertions are run by test runner thread

--- test/ringo/promise_test.js
+++ test/ringo/promise_test.js
@@ -2,16 +2,22 @@ var assert = require("assert");
 var {defer, promises} = require("ringo/promise");
 
 exports.testPromiseList = function() {
-    var d1 = defer(), d2 = defer(), d3 = defer();
+    var d1 = defer(), d2 = defer(), d3 = defer(), done = defer();
     var l = promises(d1.promise, d2.promise, d3); // promiseList should convert d3 to promise
     l.then(function(result) {
-        assert.deepEqual(result, [{value: "ok"}, {value: 1}, {error: "error"}]);
+        done.resolve(result);
     }, function(error) {
-        assert.fail("promiseList called error callback");
+        done.resolve("promises called error callback", true);
     });
     d2.resolve(1);
     d3.resolve("error", true);
     d1.resolve("ok");
+    // make sure promises resovle via wait()
+    var result = l.wait();
+    assert.deepEqual(result, [{value: "ok"}, {value: 1}, {error: "error"}]);
+    // make sure promises resolve via callback
+    result = done.promise.wait();
+    assert.deepEqual(result, [{value: "ok"}, {value: 1}, {error: "error"}]);
 };
 
 // start the test runner if we're called directly from command line
