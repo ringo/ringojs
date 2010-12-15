3ea0d11df4c11e1865fac9555b075b9ba9efbe81
Hannes Wallnöfer <hannesw@gmail.com>
2010-12-15 17:21:05 +0100

    Make sure native POSIX library gets loaded in privileged action.

--- src/org/ringojs/wrappers/POSIX.java
+++ src/org/ringojs/wrappers/POSIX.java
@@ -2,6 +2,9 @@ package org.ringojs.wrappers;
 
 import org.ringojs.util.POSIXSupport;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 /**
  * A wrapper class to allow retrieving jnr-posix support while protecting
  * the caller against NoClassDefFoundError if jnr-posix support is not installed.
@@ -9,10 +12,17 @@ import org.ringojs.util.POSIXSupport;
 public class POSIX {
 
     public static Object getPOSIX() throws ClassNotFoundException {
-        try {
-            return POSIXSupport.getPOSIX();
-        } catch (NoClassDefFoundError noclass) {
-            throw new ClassNotFoundException(noclass.getMessage(), noclass);
-        }
+        return AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            public Object run() {
+                try {
+                    org.jruby.ext.posix.POSIX posix = POSIXSupport.getPOSIX();
+                    // call some method to make sure native library gets loaded
+                    posix.getuid();
+                    return posix;
+                } catch (NoClassDefFoundError noclass) {
+                    throw new RuntimeException(noclass);
+                }
+            }
+        });
     }
 }
