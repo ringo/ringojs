46425d749db353550a98f59331adfcc3304f789c
Hannes Wallnöfer <hannesw@gmail.com>
2011-04-19 11:25:26 +0200

    Fix/add module docs.

--- modules/ringo/daemon.js
+++ modules/ringo/daemon.js
@@ -1,9 +1,9 @@
 /**
- * @fileoverview: The daemon control script invoked by the init script.
+ * @fileoverview The daemon control script invoked by the init script.
+ *
+ * This module interprets the first command line argument as module ID, load
+ * the module and try to invoke the lifecycle functions on it.
  */
-
-
-
 
 var system = require('system');
 var log = require('ringo/logging').getLogger(module.id);
@@ -12,7 +12,12 @@ var app, appId;
 
 export('init', 'start', 'stop', 'destroy');
 
-
+/**
+ * Called when the daemon instance is created.
+ *
+ * This function can be run with superuser id to perform privileged actions
+ * before the daemon is started.
+ */
 function init() {
     log.info("init", system.args);
     // Remove our own script name from args
@@ -33,6 +38,9 @@ function init() {
     }
 }
 
+/**
+ * Called when the daemon instance is started.
+ */
 function start() {
     log.info("start");
     if (app && typeof app.start === "function") {
@@ -40,6 +48,9 @@ function start() {
     }
 }
 
+/**
+ * Called when the daemon is stopped.
+ */
 function stop() {
     log.info("stop");
     if (app && typeof app.stop === "function") {
@@ -47,6 +58,9 @@ function stop() {
     }
 }
 
+/**
+ * Called when the daemon is destroyed.
+ */
 function destroy() {
     log.info("destroy");
     if (app && typeof app.destroy === "function") {
