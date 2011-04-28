fdaef66f7b42565558a8d54a446f806f1a2015b6
Hannes Wallnöfer <hannesw@gmail.com>
2011-04-28 11:12:51 +0200

    Add missing var.

--- apps/admin/skeletons/app/actions.js
+++ apps/admin/skeletons/app/actions.js
@@ -1,6 +1,6 @@
 var {htmlResponse} = require('ringo/jsgi/response');
 
 exports.index = function (req) {
-    response = getResource("./templates/index.html");
+    var response = getResource("./templates/index.html");
     return htmlResponse(response.content);
 };
