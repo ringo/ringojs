6441b918b42fe3c1d9f9fc4b6927e39ba67b8cd2
Hannes Wallnöfer <hannesw@gmail.com>
2011-05-17 10:39:17 +0200

    Handle null for SyntaxError sourceName (Rhino bug)

--- src/org/ringojs/engine/SyntaxError.java
+++ src/org/ringojs/engine/SyntaxError.java
@@ -9,7 +9,7 @@ public class SyntaxError {
 
     SyntaxError(String message, String sourceName, int line, String lineSource, int offset) {
         this.message = message;
-        this.sourceName = sourceName;
+        this.sourceName = sourceName == null ? "[unknown source]" : sourceName;
         this.lineSource = lineSource;
         this.line = line;
         this.offset = offset;
@@ -17,8 +17,8 @@ public class SyntaxError {
 
     public String toString() {
         String lineSeparator = System.getProperty("line.separator", "\n");
-        StringBuffer b = new StringBuffer(sourceName).append(", line ").append(line)
-                .append(": ").append(message).append(lineSeparator)
+        StringBuilder b = new StringBuilder(sourceName).append(", line ")
+                .append(line).append(": ").append(message).append(lineSeparator)
                 .append(lineSource).append(lineSeparator);
         for(int i = 0; i < offset - 1; i++) {
             b.append(' ');
@@ -29,9 +29,10 @@ public class SyntaxError {
 
     public String toHtml() {
         String lineSeparator = System.getProperty("line.separator", "\n");
-        StringBuffer b = new StringBuffer("<div>").append(sourceName).append(", line ")
-                .append(line).append(": <b>").append(StringUtils.escapeHtml(message))
-                .append("</b></div>").append(lineSeparator).append("<pre>");
+        StringBuilder b = new StringBuilder("<div>").append(sourceName)
+                .append(", line ").append(line).append(": <b>")
+                .append(StringUtils.escapeHtml(message)).append("</b></div>")
+                .append(lineSeparator).append("<pre>");
         int srcLength = lineSource.length();
         int errorStart = Math.max(0, offset - 2);
         int errorEnd = Math.min(srcLength, offset);
