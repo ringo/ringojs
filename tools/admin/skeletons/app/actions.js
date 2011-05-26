9e03bac76d1075ac1219097c2bf02e121729e8c5
Hannes Wallnöfer <hannesw@gmail.com>
2011-05-26 18:23:19 +0200

    Use mustache template for skeleton app

--- tools/admin/skeletons/app/actions.js
+++ tools/admin/skeletons/app/actions.js
@@ -1,6 +1,11 @@
 var response = require('ringo/jsgi/response');
+var mustache = require('ringo/mustache');
 
 exports.index = function (req) {
-    var resource = getResource("./templates/index.html");
-    return response.html(resource.content);
+    var template = getResource("./templates/index.html").content;
+    return response.html(
+        mustache.to_html(template, {
+            title: "It's working!"
+        })
+    );
 };
