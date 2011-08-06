9eb661a122bd10b5b9a63522543b39567dd37442
Jim Cook <jcook@tracermedia.com>
2011-08-06 13:50:35 -0400

    Fixing some tests which will fail under Windows.

--- test/repository/common.js
+++ test/repository/common.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
 var {absolute, join} = require("fs");
 var strings = require("ringo/utils/strings");
+var {separator} = java.io.File;
 
 exports.setup = function(exports, path, repo) {
 
@@ -9,7 +10,9 @@ exports.setup = function(exports, path, repo) {
         assert.strictEqual(res.name, "test.txt");
         assert.isTrue(res.exists());
         assert.strictEqual(res.baseName, "test");
-        assert.strictEqual(res.path, absolute(join(path, "test.txt")));
+        // Regardless of OS, when resolving paths inside an archive file the separator is '/'.
+        var comparePath = /\.zip$|\.jar$/.test(path) ? absolute(path) + "/test.txt" : absolute(join(path, "test.txt"));
+        assert.strictEqual(res.path, comparePath);
         assert.strictEqual(res.content, "hello world!");
     };
 
@@ -18,9 +21,17 @@ exports.setup = function(exports, path, repo) {
         assert.strictEqual(res.name, "nested.txt");
         assert.isTrue(res.exists());
         assert.strictEqual(res.baseName, "nested");
-        assert.strictEqual(res.path, absolute(join(path, "nested", "nested.txt")));
-        assert.strictEqual(res.length, 2240);
-        assert.strictEqual(res.content.length, 2240);
+        // Regardless of OS, when resolving paths inside an archive file the separator is '/'.
+        var comparePath = /\.zip$|\.jar$/.test(path)
+                ? absolute(path) + "/nested/nested.txt"
+                : absolute(join(path, "nested", "nested.txt"));
+        assert.strictEqual(res.path, comparePath);
+        // Windows uses two bytes for EOL while other OS's use one byte.
+        var len = /^windows/.test(java.lang.System.getProperty("os.name", "generic").toLowerCase()) ? 2274 : 2240;
+        // Except when in an archive, then it depends on what OS the archive was created, ugh.
+        if (/\.zip$|\.jar$/.test(path)) len = 2240;
+        assert.strictEqual(res.length, len);
+        assert.strictEqual(res.content.length, len);
         assert.isTrue(strings.startsWith(res.content, "Lorem ipsum dolor sit amet"));
         assert.isTrue(strings.endsWith(res.content.trim(), "id est laborum."));
     };
@@ -57,10 +68,14 @@ exports.setup = function(exports, path, repo) {
         res = res.sort(function(a, b) a.length - b.length);
         assert.strictEqual(res[0].name, "test.txt");
         assert.strictEqual(res[0].relativePath, "test.txt");
-        assert.strictEqual(res[0].path, absolute(join(path, "test.txt")));
+        // Regardless of OS, when resolving paths inside an archive file the separator is '/'.
+        var comparePath = /\.zip$|\.jar$/.test(path) ? absolute(path) + "/test.txt" : absolute(join(path, "test.txt"));
+        assert.strictEqual(res[0].path, comparePath);
         assert.strictEqual(res[1].name, "nested.txt");
         assert.strictEqual(res[1].relativePath, "nested/nested.txt");
-        assert.strictEqual(res[1].path, absolute(join(path, "nested/nested.txt")));
+        // Regardless of OS, when resolving paths inside an archive file the separator is '/'.
+        var comparePath = /\.zip$|\.jar$/.test(path) ? absolute(path) + "/nested/nested.txt" : absolute(join(path, "nested/nested.txt"));
+        assert.strictEqual(res[1].path, comparePath);
     }
 
     exports.testGetNestedResources = function() {
