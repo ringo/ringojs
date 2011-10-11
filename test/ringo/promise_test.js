d59a36f5ee64ed5baa46f46bfae23bc2375c4e25
Hannes Wallnöfer <hannesw@gmail.com>
2011-10-11 20:53:22 +0200

    Fix promise test using a worker/semaphore hack
    (looks like we need support for async tests)

--- test/ringo/promise_test.js
+++ test/ringo/promise_test.js
@@ -1,22 +1,32 @@
 var assert = require("assert");
 var {defer, promises} = require("ringo/promise");
+var {Worker} = require("ringo/worker");
+var semaphore = new java.util.concurrent.Semaphore(0);
 
 exports.testPromiseList = function() {
     var d1 = defer(), d2 = defer(), d3 = defer(), done = defer();
     var l = promises(d1.promise, d2.promise, d3); // promiseList should convert d3 to promise
-    l.then(function(result) {
-        done.resolve(result);
+    var result;
+    l.then(function(value) {
+        done.resolve(value);
     }, function(error) {
         done.resolve("promises called error callback", true);
     });
-    d2.resolve(1);
-    d3.resolve("error", true);
-    d1.resolve("ok");
-    // make sure promises resolve via wait()
-    var result = l.wait();
-    assert.deepEqual(result, [{value: "ok"}, {value: 1}, {error: "error"}]);
-    // make sure promises resolve via callback
-    result = done.promise.wait();
+    done.promise.then(function(value) {
+        result = value;
+        semaphore.release();
+    });
+    // Resolve promise in worker so async promise callbacks get called
+    var worker = new Worker({
+        onmessage: function() {
+            d2.resolve(1);
+            d3.resolve("error", true);
+            d1.resolve("ok");
+        }
+    }).postMessage();
+    // wait for last promise to resolve
+    semaphore.acquire();
+    // make sure promises have resolved via chained callback
     assert.deepEqual(result, [{value: "ok"}, {value: 1}, {error: "error"}]);
 };
 
