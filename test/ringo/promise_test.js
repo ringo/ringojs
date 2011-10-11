6b46526d1987a864b13c6e928a72e2c6086b43b4
Hannes Wallnöfer <hannesw@gmail.com>
2011-10-11 21:37:33 +0200

    Add propert timeouts to async tests

--- test/ringo/promise_test.js
+++ test/ringo/promise_test.js
@@ -1,10 +1,11 @@
 var assert = require("assert");
 var {defer, promises} = require("ringo/promise");
 var {Worker} = require("ringo/worker");
+var {Semaphore, TimeUnit} = java.util.concurrent;
 
 exports.testPromise = function() {
     var d1 = defer(), d2 = defer();
-    var semaphore = new java.util.concurrent.Semaphore(0);
+    var semaphore = new Semaphore(0);
     var v1, v2, e2;
     d1.promise.then(function(value) {
         v1 = value;
@@ -17,24 +18,28 @@ exports.testPromise = function() {
         semaphore.release();
     });
     // Resolve promise in worker so async promise callbacks get called
-    new Worker({
+    var worker = new Worker({
         onmessage: function() {
             d1.resolve("value");
             d2.resolve("error", true);
         }
-    }).postMessage();
+    });
+    worker.postMessage();
     // wait for promises to resolve
-    semaphore.acquire(2);
+    if (!semaphore.tryAcquire(2, 1000, TimeUnit.MILLISECONDS)) {
+        assert.fail("timed out");
+    }
     // make sure promises have resolved via chained callback
     assert.equal(v1, "value");
     assert.equal(v2, undefined);
     assert.equal(e2, "error");
+    worker.terminate();
 };
 
 exports.testPromiseList = function() {
     var d1 = defer(), d2 = defer(), d3 = defer(), done = defer();
     var l = promises(d1.promise, d2.promise, d3); // promiseList should convert d3 to promise
-    var semaphore = new java.util.concurrent.Semaphore(0);
+    var semaphore = new Semaphore(0);
     var result;
     l.then(function(value) {
         done.resolve(value);
@@ -46,17 +51,21 @@ exports.testPromiseList = function() {
         semaphore.release();
     });
     // Resolve promise in worker so async promise callbacks get called
-    new Worker({
+    var worker = new Worker({
         onmessage: function() {
             d2.resolve(1);
             d3.resolve("error", true);
             d1.resolve("ok");
         }
-    }).postMessage();
+    });
+    worker.postMessage();
     // wait for last promise to resolve
-    semaphore.acquire();
+    if (!semaphore.tryAcquire(1000, TimeUnit.MILLISECONDS)) {
+        assert.fail("timed out");
+    }
     // make sure promises have resolved via chained callback
     assert.deepEqual(result, [{value: "ok"}, {value: 1}, {error: "error"}]);
+    worker.terminate();
 };
 
 // start the test runner if we're called directly from command line
