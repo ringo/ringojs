60f95fc762e7c7d5aa2dad9de314462c9ad37247
Hannes Wallnöfer <hannesw@gmail.com>
2012-02-07 11:41:41 +0100

    Consolidate markdown module into a single `process` function.
    MarkdownProcessor objects are not reusable, so there's little point in
    exporting the constructor, and this makes the interface much simpler.

--- modules/ringo/markdown.js
+++ modules/ringo/markdown.js
@@ -6,41 +6,29 @@
 /**
  * Converts a string of Markdown formatted text to HTML.
  *
+ * Passing in an optional JavaScript object as argument allows the caller to
+ * override behaviour in the markdown processor. Specifically, the following
+ * methods can be overridden:
+ *
+ *  - __getLink(id)__ called to resolve Markdown link ids. Takes a single
+ *     string id as argument and must return an array containing the target
+ *     link and the target link title. If this returns null, the markdown link
+ *     will not be rendered as HTML link.
+ *
+ *  - __openTag(tagname, buffer)__ called when a HTML tag is opened. `tagname`
+ *     is an HTML tag such as `pre` or `div`, buffer is a java.lang.StringBuffer
+ *     to append to. The function can be used to create HTML tags with
+ *     additional attributes.
+ *
  * @param {String} text a Markdown formatted text
+ * @param {Object} [extension] optional object with methods overriding default
+ * behaviour in org.ringojs.util.MarkdownProcessor
  * @returns {String} the Markdown text converted to HTML
  */
-exports.process = function(text) {
-    var md = new org.ringojs.util.MarkdownProcessor();
+exports.process = function(text, extension) {
+    var MarkdownProcessor = org.ringojs.util.MarkdownProcessor;
+    var md = extension ? new JavaAdapter(MarkdownProcessor, extension)
+                       : new MarkdownProcessor();
     return md.process(text);
 }
 
-/**
- * Get an instance of org.ringojs.util.MarkdownProcessor. Passing in an
- * optional JavaScript object as argument allows the caller to override some
- * methods in that class. Specifically, the following methods can be overridden:
- *
- *  - **getLink(id)** called to resolve Markdown link ids. Takes a single string id
- *     as argument and must return an array containing the target link and the
- *     target link title. If this returns null, the markdown link will not be
- *     rendered as HTML link.
- *
- *  - **openTag(tagname, buffer)** called when a HTML tag is opened. `tagname` is
- *     an HTML tag such as `pre` or `div`, buffer is a java.lang.StringBuffer to append to.
- *     The function can be used to create HTML tags with additional attributes.
- *
- * @param {Object} extension optional object with methods overriding default
- * behaviour in org.ringojs.util.MarkdownProcessor class
- */
-exports.Markdown = function Markdown(extension) {
-    extension = extension || {};
-    return new JavaAdapter(org.ringojs.util.MarkdownProcessor, extension);
-};
-
-/**
- * Converts a string of Markdown formatted text to HTML.
- *
- * @param {String} text Markdown formatted text
- * @returns {String} the Markdown text converted to HTML
- * @function
- * @name Markdown.instance.process
- */
