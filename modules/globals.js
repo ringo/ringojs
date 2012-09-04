a6b641a6693b696c9c1240464c4efa011c56ac57
Hannes Wallnöfer <hannesw@gmail.com>
2012-09-04 18:14:10 +0200

    Add first batch of API docs to new globals module

--- modules/globals.js
+++ modules/globals.js
@@ -1,16 +1,115 @@
 /**
- * @fileoverview <p>This module provides the RingoJS-specific global functions
- * import, include, and export, as well as require.loader for Narwhal
- * compatibility.</p>
+ * @fileoverview RingoJS adopts some of the global properties from the Rhino
+ * shell and adds a few of its own.
+ *
+ * Note that this module must and can not be imported like an ordinary module.
+ * It is evaluated only once upon RingoJS startup.
  */
 
+/**
+ * A reference to the global object itself.
+ *
+ * When a module is evaluated in RingoJS it uses its own private module scope
+ * which in turn uses this shared global object as prototype. Therefore,
+ * properties of the global object are visible in every module.
+ *
+ * Since the global object is hidden in the prototype chain of module scopes
+ * it cannot normally be accessed directly. This reference allows you to do so,
+ * defining real global variables if you want to do so.
+ *
+ * @name global
+ */
 Object.defineProperty(this, "global", { value: this });
+
+/**
+ * The `require` function as defined in the
+ * [CommonJS Modules 1.1.1](http://wiki.commonjs.org/wiki/Modules/1.1.1)
+ * specification.
+ *
+ * The RingoJS `require` function has the following properties:
+ *
+ *  - [extensions][#require.extensions]
+ *  - [main][#require.main]
+ *  - [paths][#require.paths]
+ *
+ * @param {String} moduleId the id or path of the module to load
+ * @return {Object} the exports object of the required module
+ * @name require
+ * @function
+ */
+
+/**
+ *  An object used to extend the way [require][#require] loads modules.
+ *
+ *  Use a file extension as key and a function as value. The function should
+ *  accept a `Resource` object as argument and return either a string to be
+ *  used as JavaScript module source or an object which will be directly
+ *  returned by `require`.
+ *
+ *  For example, the following one-liner will enable `require()` to load XML
+ *  files as E4X modules:
+ *
+ *     require.extensions['.xml'] = function(r) new XML(r.content);
+ *
+ * @name require.extensions
+ */
+
+/**
+ *  If RingoJS was started with a command line script, `require.main` contains
+ *  the `module` object of the main module. Otherwise, this property is defined
+ *  but has the value `undefined`.
+ *
+ * @name require.main
+ */
+
+/**
+ *  An array that contains the module search path. You can add or
+ *  remove paths items to or from this array in order to change the places
+ *  where RingoJS will look for modules.
+ * @name require.paths
+ */
+
+/**
+ * The Module object as defined in the
+ * [CommonJS Modules 1.1.1](http://wiki.commonjs.org/wiki/Modules/1.1.1)
+ * specification.
+ *
+ * The RingoJS `module` object has the following properties:
+ *
+ *   - id
+ *   - path
+ *   - uri
+ *   - [resolve][#module.resolve]
+ *   - [singleton][#module.singleton]
+ *
+ * @name module
+ * @property module
+ */
+
+/**
+ * @param {String} path
+ * @name module.resolve
+ * @function
+ */
+
+/**
+ * @param {String} id
+ * @param {Function} factory
+ * @name module.singleton
+ * @function
+ */
+
+/**
+ * @name exports
+ */
 
 (function() {
 
     /**
      * Load a module and include all its properties in the calling scope.
-     * @param {String} moduleName the module name such as 'core.object'
+     * @param {String} moduleId  the id or path of the module to load
+     * @name include
+     * @function
      */
     Object.defineProperty(this, "include", {
         value: function(moduleName) {
@@ -22,8 +121,14 @@ Object.defineProperty(this, "global", { value: this });
     });
 
     /**
-     * Define the properties to be exported.
-     * @param name one or more names of exported properties
+     * Takes any number of top-level names to be exported from this module.
+     *
+     * This is a non-standard alternative to the [exports][#exports] object
+     * for exporting values in a less verbose and intrusive way.
+     *
+     * @param name... one or more names of exported properties
+     * @name export
+     * @function
      */
     Object.defineProperty(this, "export", {
         value: function() {
@@ -45,10 +150,18 @@ Object.defineProperty(this, "global", { value: this });
         }
     });
 
-    // preload system module
+    /**
+     * The `system` module, preloaded for your convenience.
+     * @name system
+     * @see system
+     */
     var system = this.system = this.system || require('system');
 
-    // Firebug-like debug console
+    /**
+     * Firebug-like debug console, preloaded for your convenience.
+     * @name console
+     * @see console
+     */
     Object.defineProperty(this, "console", {
         get: function() require("console"),
         enumerable: true
