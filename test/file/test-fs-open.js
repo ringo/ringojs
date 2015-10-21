1c89c7ce877d0f650794fb016937c36dfe9e1d34
Philipp Naderer <philipp.naderer@gmail.com>
2015-10-21 22:45:08 +0200

    Improves the fs.open() tests

--- test/file/test-fs-open.js
+++ test/file/test-fs-open.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var fs = require("fs");
+var {ByteArray} = require("binary");
 
 var testFile = fs.join(java.lang.System.getProperty("java.io.tmpdir"), "opentest.txt");
 
@@ -36,6 +37,77 @@ exports.testOpenRaw = function() {
         });
     } catch (err) {
         assert.strictEqual(err.message, "Cannot open a file for reading and writing at the same time");
+    }
+};
+
+exports.testOpenAndWrite = function() {
+    assert.isTrue(fs.exists(testFile));
+    var stream = fs.open(testFile, "wb");
+
+    var ba = new ByteArray([0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
+
+    try {
+        stream.write(ba);
+        stream.flush();
+    } finally {
+        stream.close();
+        assert.isTrue(stream.closed());
+    }
+
+    stream = fs.open(testFile, "rb");
+    try {
+        var buff = new ByteArray(10);
+        assert.equal(stream.readInto(buff), 10);
+
+        // Check the content
+        for (var i = 0; i < buff.length; i++) {
+            assert.equal(buff.charCodeAt(i), ba.charCodeAt(i));
+        }
+    } finally {
+        stream.close();
+        assert.isTrue(stream.closed());
+    }
+};
+
+exports.testOpenAndAppend = function() {
+    var ba = new ByteArray([0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
+
+    assert.isTrue(fs.exists(testFile));
+    var stream = fs.open(testFile, "ab");
+
+    try {
+        stream.write(ba);
+        stream.flush();
+    } finally {
+        stream.close();
+        assert.isTrue(stream.closed());
+    }
+
+    stream = fs.open(testFile, "ab");
+
+    try {
+        stream.write(ba);
+        stream.flush();
+    } finally {
+        stream.close();
+        assert.isTrue(stream.closed());
+    }
+
+    stream = fs.open(testFile, "rb");
+    try {
+        var buff = new ByteArray(20);
+        assert.equal(stream.readInto(buff), 20);
+
+        // Check the content
+        for (var i = 0; i < 10; i++) {
+            assert.equal(buff.charCodeAt(i), ba.charCodeAt(i));
+        }
+        for (var u = 10; u < 20; u++) {
+            assert.equal(buff.charCodeAt(i), ba.charCodeAt(i - 10));
+        }
+    } finally {
+        stream.close();
+        assert.isTrue(stream.closed());
     }
 };
 
