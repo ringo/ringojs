56788e988db4f1a271b6b73da2a6c45af5d9d8c7
Philipp Naderer <philipp.naderer@gmail.com>
2015-11-09 23:51:20 +0100

    Improves and extends the fs.join() test

--- test/file/test-fs-join.js
+++ test/file/test-fs-join.js
@@ -1,14 +1,39 @@
 var assert = require("assert");
 var fs = require('fs');
-var separator = require('ringo/utils/files').separator;
+var SEP = require('ringo/utils/files').separator;
 
-exports.testJoin = function() {
-    assert.equal(fs.join("foo", "bar", "baz"), ["foo", "bar", "baz"].join(separator));
-    assert.equal(fs.join("foo" + separator, "bar" + separator + separator, "baz"), ["foo", "bar", "baz"].join(separator));
-    assert.equal(fs.join("foo", "..", "bar"), "foo" + separator + ".." + separator + "bar");
-    assert.equal(fs.join("foo", ".", "bar"), "foo" + separator + "." + separator + "bar");
-    assert.equal(fs.join(separator, "foo", ".", "bar"), separator + "foo" + separator + "." + separator + "bar");
-};
+var tests = [
+    // actual value                             expected
+    ["foo", "bar", "baz.js"],                   ["foo", "bar", "baz.js"].join(SEP),
+    ["foo", "bar", ".baz.js"],                  ["foo", "bar", ".baz.js"].join(SEP),
+    ["foo", "bar", ".baz"],                     ["foo", "bar", ".baz"].join(SEP),
+    ["", ".baz"],                               (".baz"),
+    ["", "", ".baz"],                           (".baz"),
+    ["foo", "bar", "baz"],                      ["foo", "bar", "baz"].join(SEP),
+    ["foo" + SEP, "bar" + SEP + SEP, "baz"] ,   ["foo", "bar", "baz"].join(SEP),
+    ["foo", "..", "bar"],                       ("foo" + SEP + ".." + SEP + "bar"),
+    ["foo", ".", "bar"],                        ("foo" + SEP + "." + SEP + "bar"),
+    [SEP, "foo", ".", "bar"],                   (SEP + "foo" + SEP + "." + SEP + "bar"),
+    ["foo" + SEP, ""],                          ("foo"),
+    ["", SEP, "foo" + SEP, ""],                 (SEP + "foo"),
+    [" ", "."],                                 (" " + SEP + "."),
+    [" ", ".."],                                (" " + SEP + ".."),
+    [SEP, SEP + "foo"],                         (SEP + "foo"),
+    [SEP, SEP, SEP + "foo"],                    (SEP + "foo"),
+    [SEP, SEP, SEP + SEP + "foo"],              (SEP + "foo"),
+    ["." + SEP, "..", SEP + "foo"],             ("." + SEP + ".." + SEP + "foo"),
+    ["." + SEP, "..", "..", SEP + "foo"],       ("." + SEP + ".." + SEP + ".." + SEP + "foo"),
+    [".", "..", "..", SEP + "foo"],             ("." + SEP + ".." + SEP + ".." + SEP + "foo"),
+    ["", "", "", ""],                           (".")
+];
+
+tests.forEach(function(item, index, arr) {
+    if (index % 2 == 0) {
+        exports["test fs.join('" + item.join("', '") + "')"] = function() {
+            assert.equal(fs.join.apply(this, item), arr[index + 1]);
+        }
+    }
+});
 
 if (require.main == module.id) {
     require('system').exit(require("test").run(module.id));
