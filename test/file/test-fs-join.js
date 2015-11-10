e1072f5da8bfff6fd6cc1a274d5404e9c19576d1
Philipp <philipp.naderer@gmail.com>
2015-11-10 11:23:07 +0100

    Adds Windows-specific tests for fs.join()

--- test/file/test-fs-join.js
+++ test/file/test-fs-join.js
@@ -2,6 +2,9 @@ var assert = require("assert");
 var fs = require('fs');
 var SEP = require('ringo/utils/files').separator;
 
+var POSIX = java.nio.file.FileSystems.getDefault().supportedFileAttributeViews().contains("posix");
+var WIN32 = java.nio.file.FileSystems.getDefault().supportedFileAttributeViews().contains("dos");
+
 var tests = [
     // actual value                             expected
     ["foo", "bar", "baz.js"],                   ["foo", "bar", "baz.js"].join(SEP),
@@ -13,27 +16,63 @@ var tests = [
     ["foo" + SEP, "bar" + SEP + SEP, "baz"] ,   ["foo", "bar", "baz"].join(SEP),
     ["foo", "..", "bar"],                       ("foo" + SEP + ".." + SEP + "bar"),
     ["foo", ".", "bar"],                        ("foo" + SEP + "." + SEP + "bar"),
-    [SEP, "foo", ".", "bar"],                   (SEP + "foo" + SEP + "." + SEP + "bar"),
     ["foo" + SEP, ""],                          ("foo"),
-    ["", SEP, "foo" + SEP, ""],                 (SEP + "foo"),
-    [" ", "."],                                 (" " + SEP + "."),
-    [" ", ".."],                                (" " + SEP + ".."),
-    [SEP, SEP + "foo"],                         (SEP + "foo"),
-    [SEP, SEP, SEP + "foo"],                    (SEP + "foo"),
-    [SEP, SEP, SEP + SEP + "foo"],              (SEP + "foo"),
     ["." + SEP, "..", SEP + "foo"],             ("." + SEP + ".." + SEP + "foo"),
     ["." + SEP, "..", "..", SEP + "foo"],       ("." + SEP + ".." + SEP + ".." + SEP + "foo"),
     [".", "..", "..", SEP + "foo"],             ("." + SEP + ".." + SEP + ".." + SEP + "foo"),
     ["", "", "", ""],                           (".")
 ];
 
-tests.forEach(function(item, index, arr) {
+var posixSpecific = [
+    [" ", "."],                                 (" /."),
+    [" ", ".."],                                (" /.."),
+    ["", "/", "foo/", ""],                      ("/foo"),
+    ["/", "foo", ".", "bar"],                   ("/foo/./bar"),
+    ["/", "/" + "foo"],                         ("/foo"),
+    ["/", "/", "/foo"],                         ("/foo"),
+    ["/", "/", "//foo"],                        ("/foo")
+];
+
+var windowsSpecific = [
+    [""],                                       ("."),
+    ["C:\\"],                                   ("C:\\"),
+    ["C:"],                                     ("C:"),
+    ["C:tmp.txt"],                              ("C:tmp.txt"),
+    ["C:dir","tmp.txt"],                        ("C:dir\\tmp.txt"),
+    ["C:\\tmp.txt"],                            ("C:\\tmp.txt"),
+    ["C:\\dir", "file"],                        ("C:\\dir\\file"),
+    ["C:\\dir", ".file"],                       ("C:\\dir\\.file"),
+    ["C:\\dir", "file.txt"],                    ("C:\\dir\\file.txt"),
+    ["\\\\server", "share"],                    ("\\\\server\\share\\"),
+    ["\\\\server", "share", ""],                ("\\\\server\\share\\"),
+    ["", "\\\\server", "share"],                ("\\\\server\\share\\"),
+    ["\\\\server", "share", "folder"],          ("\\\\server\\share\\folder"),
+    ["\\\\127.0.0.1", "a"],                     ("\\\\127.0.0.1\\a\\"),
+    ["\\\\2001-db8--2468-1357-abcd-3456.ipv6-literal.net", "a"], ("\\\\2001-db8--2468-1357-abcd-3456.ipv6-literal.net\\a\\")
+];
+
+
+// adds given test to the exports object
+var testHelper = function(item, index, arr) {
     if (index % 2 == 0) {
         exports["test fs.join('" + item.join("', '") + "')"] = function() {
             assert.equal(fs.join.apply(this, item), arr[index + 1]);
         }
     }
-});
+};
+
+// common test for all OS
+tests.forEach(testHelper);
+
+// POSIX / Unix FS tests
+if (POSIX) {
+    posixSpecific.forEach(testHelper);
+}
+
+// Windows / DOS tests
+if (WIN32) {
+    windowsSpecific.forEach(testHelper);
+}
 
 if (require.main == module.id) {
     require('system').exit(require("test").run(module.id));
