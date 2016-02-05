2b5baf2696a065d58568524ff49c02a42e3151d9
Philipp Naderer <philipp.naderer@gmail.com>
2016-02-05 20:58:06 +0100

    Adds http.parseParameters() test

--- test/ringo/utils/http_test.js
+++ test/ringo/utils/http_test.js
@@ -11,6 +11,55 @@ exports.testUrlEncode = function() {
     assert.strictEqual(encoded, expected);
 };
 
+exports.testParseParameters = function() {
+    // [test, expectedResult]
+    const testCases = [
+        ["a=1", { a: "1"}],
+        ["a=1&a=1", { a: "1"}],
+        ["a=1&a=2", { a: "2"}],
+        ["a%20b=c%20d", { "a b": "c d"}],
+        ["a=b&c=d", { a: "b", c: "d"}],
+        ["a[]=b&a[]=c&a[]=d", { a: ["b", "c", "d"]}],
+        ["a=日本語0123456789日本語カタカナひらがな", { a: "日本語0123456789日本語カタカナひらがな"}],
+        [
+            "a=http%3A%2F%2Fringojs.org%2F%20foo%20bar%20buzz%2F!a%26b%3Dc",
+            { a: "http://ringojs.org/ foo bar buzz/!a&b=c"}
+        ],
+        ["a%3D=b", { "a=": "b" }],
+        ["a=a%3Db", { a: "a=b" }],
+        ["a=1&empty1=&empty2=", { a: "1", empty1: "", empty2: ""}],
+        ["a=1&empty1=&undefined", { a: "1", empty1: ""}],
+
+        // note: this is different to node, which parses it as { a: "", b: "1" }
+        // ringojs drops empty parameters!
+        ["a&b=1", { b: "1"}],
+
+        ["foo[bar][baz]=hello", {foo: {bar: {baz: "hello"}}}],
+        [
+            "foo[bar][baz]=hello&foo[bor][baz]=world",
+            {foo: {bar: {baz: "hello"}, bor: {baz: "world"}}}
+        ],
+        [
+            "foo[bar][baz]=http%3A%2F%2Fringojs.org%2F%20foo%20bar%20buzz%2F!a%26b%3Dc",
+            {foo: {bar: {baz: "http://ringojs.org/ foo bar buzz/!a&b=c"}}}
+        ],
+        ["foo[bar][][baz]=hello", {foo: {bar: [{baz: "hello"}]}}],
+        ["foo[bar]asdf[baz]=hello", {"foo[bar]asdf[baz]": "hello"}],
+        ["foo=%F0%9D%8C%86-%E2%98%83%F0%9F%98%80", { foo: "\uD834\uDF06-\u2603\uD83D\uDE00" }],
+
+        // See: https://github.com/nodejs/node/master/test/parallel/test-querystring.js
+        // See: https://github.com/joyent/node/issues/1707
+        ["toString=12345", { "toString": "12345"}],
+        ["hasOwnProperty=12345", { "hasOwnProperty": "12345"}],
+        ["__defineGetter__=12345", { "__defineGetter__": "12345"}],
+        ["valueOf=12345", { "valueOf": "12345"}]
+    ];
+
+    testCases.forEach(function(test, index) {
+        assert.deepEqual(http.parseParameters(test[0]), test[1], "testCase[" + index + "] failed!");
+    });
+};
+
 if (require.main === module) {
     require('system').exit(require("test").run(module.id));
 }
