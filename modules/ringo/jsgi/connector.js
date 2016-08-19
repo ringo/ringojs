9470ae888b7da55daf70723c69605eab4e71c427
Philipp Naderer <philipp.naderer@gmail.com>
2016-08-19 21:54:58 +0200

    Allows input to be set

--- modules/ringo/jsgi/connector.js
+++ modules/ringo/jsgi/connector.js
@@ -68,6 +68,11 @@ function initRequest(request) {
                 input = new Stream(request.env.servletRequest.getInputStream());
             return input;
         },
+        set: function(stream) {
+            if (stream instanceof Stream) {
+               input = stream;
+            }
+        },
         enumerable: true
     });
     Object.defineProperty(request.jsgi, "errors", {
