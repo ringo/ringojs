2d873f797151e542bd7c757f2484c94c2c80bba3
Philipp Naderer <philipp.naderer@gmail.com>
2017-02-22 15:49:33 +0100

    Bumps version to 1.1.1

--- src/org/ringojs/engine/RhinoEngine.java
+++ src/org/ringojs/engine/RhinoEngine.java
@@ -76,7 +76,7 @@ public class RhinoEngine implements ScopeProvider {
     private static Logger log = Logger.getLogger(RhinoEngine.class.getName());
 
     public static final List<Integer> VERSION =
-            Collections.unmodifiableList(Arrays.asList(1, 1, 0));
+            Collections.unmodifiableList(Arrays.asList(1, 1, 1));
 
     /**
      * Create a RhinoEngine with the given configuration. If <code>globals</code>
