7cb0d39b664ff70fb27fb38f55819cf20d3c8f41
Simon Oberhammer <simon.oberhammer@orf.at>
2017-11-06 10:34:07 +0100

    add basic test for ringo interactive shell

--- test/integration-tests/all.js
+++ test/integration-tests/all.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var assert = require('assert');
 var system = require('system');
-var {createProcess} = require('ringo/subprocess');
+var {createProcess, command} = require('ringo/subprocess');
 
 const RINGO_BIN = (function() {
    var bin = 'ringo';
@@ -23,6 +23,41 @@ exports.testJarsToClasspath = function() {
    // wait for the exit code
    assert.equal(process.wait(), 0);
 };
+
+const testInOut = [
+   {
+      in: 'console.log("Hello World");',
+      out: 'Hello World'
+   },
+   {
+      in: 'console.log(0.1 + 0.2);',
+      out: '0.30000000000000004'
+   },
+   {
+      in: 'console.log(typeof require("fs"));',
+      out: 'object'
+   }
+];
+
+exports.testInteractiveShell = function() {
+   const stdOutVersion = command(RINGO_BIN, '--version');
+   assert.equal(stdOutVersion.indexOf('RingoJS'), 0, 'ringo --version returns version number');
+
+   const process = createProcess({
+      command: [RINGO_BIN, '--silent']
+   });
+   const processIn = process.stdin;
+   const processOut = process.stdout;
+   testInOut.forEach(function(test, idx) {
+      processIn
+         .writeLine(test.in)
+         .flush();
+      // ignore terminal ESC and clear characters as well as newline at end
+      // as long as what we want is printed at start of the line.
+      assert.equal(processOut.readLine().indexOf(test.out), 0, 'testInOut ' + idx + ' should match');
+   });
+   process.kill();
+}
 
 exports.testRequireMain = require("./require-index/main").testCalculator;
 exports.testHttpJsgiBinding = require("./http-jsgi-binding/simple").testHttpJsgiBinding;
