272dc5f8b2ad510c0fdf6fe1e8f31308a08837a2
Robert Gaggl <robert@nomatic.org>
2020-07-31 10:56:21 +0200

    made RingoSecurityManager java 11 compliant by removing checkMemberAccess()
    honestly i don't know if anyone uses ringo with security policies, so this
    coud possibly be removed as well. for now it at least compiles with java 11

--- src/org/ringojs/security/RingoSecurityManager.java
+++ src/org/ringojs/security/RingoSecurityManager.java
@@ -9,8 +9,6 @@ public class RingoSecurityManager extends RhinoSecurityManager {
     public static final Permission
             GET_CLASSLOADER = new RuntimePermission("getClassLoader");
     public static final Permission
-            ACCESS_DECLARED_MEMBERS = new RuntimePermission("accessDeclaredMembers");
-    public static final Permission
             ACCESS_JAVA = new RingoRuntimePermission("accessJava");
     public static final Permission
             SPAWN_THREAD = new RingoRuntimePermission("spawnThread");
@@ -28,44 +26,6 @@ public class RingoSecurityManager extends RhinoSecurityManager {
     }
 
     /**
-     * Override to decide on top-most application script class instead of java class.
-     * This is currently not as useful as it should be because of Rhino's caching of
-     * reflection meta-data. Because of this, classes that have already been used
-     * by trusted code are accessible to untrusted code as well.
-     * @param clazz the class that reflection is to be performed on.
-     * @param which type of access, PUBLIC or DECLARED.
-     * @exception  SecurityException if the caller does not have
-     *             permission to access members.
-     * @exception  NullPointerException if the <code>clazz</code> argument is
-     *             <code>null</code>.
-     */
-    @Override
-    public void checkMemberAccess(final Class<?> clazz, int which) {
-        if (clazz == null) {
-            throw new NullPointerException("class can't be null");
-        }
-        if (clazz.getClassLoader() == null) {
-            return;
-        }
-        final Class c = getCurrentScriptClass();
-        if (c != null && clazz.getClassLoader() == c.getClassLoader()) {
-            return;
-        }
-        Boolean allowed = AccessController.doPrivileged(new PrivilegedAction<Boolean>() {
-            public Boolean run() {
-                ProtectionDomain pd = c == null ? null : c.getProtectionDomain();
-                return pd != null
-                        && Policy.getPolicy().implies(pd, ACCESS_DECLARED_MEMBERS) ?
-                    Boolean.TRUE : Boolean.FALSE;
-            }
-        });
-        if (allowed.booleanValue()) {
-            return;
-        }
-        checkPermission(ACCESS_DECLARED_MEMBERS);
-    }
-
-    /**
      * Check if the  top-most application script has permission to access
      * members of Java objects and classes.
      * <p>
@@ -77,20 +37,18 @@ public class RingoSecurityManager extends RhinoSecurityManager {
      */
     public void checkJavaAccess() {
 
-        final Class c = getCurrentScriptClass();
+        final Class<?> c = getCurrentScriptClass();
         if (c == null) {
             return;
         }
 
-        Boolean allowed = AccessController.doPrivileged(new PrivilegedAction<Boolean>() {
-            public Boolean run() {
-                ProtectionDomain pd = c.getProtectionDomain();
-                return pd == null || Policy.getPolicy().implies(pd, ACCESS_JAVA) ?
-                        Boolean.TRUE : Boolean.FALSE;
-            }
+        Boolean allowed = AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> {
+            ProtectionDomain pd = c.getProtectionDomain();
+            return pd == null || Policy.getPolicy().implies(pd, ACCESS_JAVA) ?
+                    Boolean.TRUE : Boolean.FALSE;
         });
 
-        if (!allowed.booleanValue()) {
+        if (!allowed) {
             throw new AccessControlException("Java access denied", ACCESS_JAVA);
         }
     }
