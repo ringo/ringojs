305f511370968e473f95ce68495357bce7eaba47
Robert Gaggl <robert.gaggl@orf.at>
2020-11-02 12:52:52 +0100

    code modernization (ie. var -> const/let)

--- modules/ringo/shell.js
+++ modules/ringo/shell.js
@@ -7,101 +7,21 @@
  *
  * @example #!/usr/bin/env ringo
  * if (require.main == module) {
- *   var obj = { 'foo': 'bar' };
+ *   const obj = { 'foo': 'bar' };
  *   require('ringo/shell').start();
  * }
  *
  * // running the script opens a new shell
  * // with the prepared object available
- * >> console.log(obj);
+ * >> console.dir(obj);
  * { foo: 'bar' }
  */
 
-var system = require('system');
-var term = require('ringo/term');
+const system = require('system');
+const term = require('ringo/term');
+const {Character} = java.lang;
 
-try {
-    var input = new Packages.jline.console.ConsoleReader();
-} catch (x) {
-    // fall back to plain standard input
-}
-
-/**
- * Write 0..n arguments to standard output.
- * @param {*...} args...
- */
-function write() {
-    var length = arguments.length;
-    var out = system.stdout;
-    for (var i = 0; i < length; i++) {
-        out.write(String(arguments[i]));
-        if (i < length - 1)
-            out.write(' ');
-    }
-}
-
-/**
- * Write 0..n arguments to standard output, followed by a newline.
- * @param {*...} args...
- */
-function writeln() {
-    write.apply(this, arguments);
-    system.stdout.writeLine('');
-}
-
-/**
- * Read a single character from the standard input.
- */
-function read() {
-    if (!input) {
-        throw new Error('jline not installed');
-    }
-    return String.fromCharCode(input.readVirtualKey());
-}
-
-/**
- * Read a single line from the standard input.
- * @param {String} prompt optional prompt to display
- * @param {String} echoChar character to use as echo,
- *         e.g. '*' for passwords or '' for no echo.
- */
-function readln(prompt, echoChar) {
-    prompt = prompt || '';
-    if (input) {
-        if (typeof echoChar == 'string') {
-            var echo = echoChar == '' ?
-                   new java.lang.Character(0) :
-                   new java.lang.Character(echoChar.charCodeAt(0));
-            return input.readLine(prompt, echo);
-        }
-        return input.readLine(prompt);
-    } else {
-        system.stdout.write(prompt);
-        return system.stdin.readLine().trim();
-    }
-}
-
-/**
- * Start the shell programmatically. This uses the current thread and thus will not
- * return. You should therefore call this function as the last statement in your script.
- * Terminating the shell will exit the program.
- * @since 0.5
- * @param {RhinoEngine} engine
- */
-function start(engine) {
-    engine = engine || require('ringo/engine').getRhinoEngine();
-    new org.ringojs.tools.RingoShell(engine).run();
-}
-
-/**
- * Quit the shell and exit the JVM.
- * @param {Number} status optional integer exit status code (default is 0)
- */
-function quit(status) {
-   java.lang.System.exit(status || 0);
-}
-
-var styles = {
+const styles = {
     'number': term.BLUE,
     'string': term.GREEN,
     'function': term.CYAN,
@@ -110,11 +30,93 @@ var styles = {
     'date': term.MAGENTA,
     'java': term.MAGENTA,
     'custom': term.RED
+};
+
+let input = null;
+try {
+    input = new Packages.jline.console.ConsoleReader();
+} catch (x) {
+    // fall back to plain standard input
 }
 
-function convert(value, nesting, visited) {
-    var type = typeof value;
-    var retval = {type: type};
+/**
+ * Write 0..n arguments to standard output.
+ * @param {*...} args...
+ */
+const write = exports.write = function() {
+    const length = arguments.length;
+    const out = system.stdout;
+    for (let i = 0; i < length; i++) {
+        out.write(String(arguments[i]));
+        if (i < length - 1) {
+            out.write(' ');
+        }
+    }
+};
+
+/**
+ * Write 0..n arguments to standard output, followed by a newline.
+ * @param {*...} args...
+ */
+exports.writeln = function() {
+    write.apply(this, arguments);
+    system.stdout.writeLine('');
+};
+
+/**
+ * Read a single character from the standard input.
+ */
+exports.read = () => {
+    if (!input) {
+        throw new Error('jline not installed');
+    }
+    return String.fromCharCode(input.readVirtualKey());
+};
+
+/**
+ * Read a single line from the standard input.
+ * @param {String} prompt optional prompt to display
+ * @param {String} echoChar character to use as echo,
+ *         e.g. '*' for passwords or '' for no echo.
+ */
+exports.readln = (prompt, echoChar) => {
+    prompt || (prompt = '');
+    if (input) {
+        if (typeof echoChar === 'string') {
+            const echo = new Character((echoChar === '') ? 0 : echoChar.charCodeAt(0));
+            return input.readLine(prompt, echo);
+        }
+        return input.readLine(prompt);
+    } else {
+        system.stdout.write(prompt);
+        return system.stdin.readLine().trim();
+    }
+};
+
+/**
+ * Start the shell programmatically. This uses the current thread and thus will not
+ * return. You should therefore call this function as the last statement in your script.
+ * Terminating the shell will exit the program.
+ * @since 0.5
+ * @param {RhinoEngine} engine
+ */
+exports.start = (engine) => {
+    engine = engine || require('ringo/engine').getRhinoEngine();
+    (new org.ringojs.tools.RingoShell(engine)).run();
+};
+
+/**
+ * Quit the shell and exit the JVM.
+ * @param {Number} status optional integer exit status code (default is 0)
+ */
+exports.quit = (status) => {
+   java.lang.System.exit(status || 0);
+};
+
+const convert = (value, nesting, visited) => {
+    const type = typeof value;
+    let retval = {type: type};
+    let count = 0;
     switch (type) {
         case 'number':
             retval.string = String(value);
@@ -152,9 +154,8 @@ function convert(value, nesting, visited) {
                 } else {
                     retval.type = "array";
                     retval.items = [];
-                    var count = 0;
-                    for (var i = 0; i < value.length; i++) {
-                        var part = convert(value[i], nesting + 1, visited);
+                    for (let i = 0; i < value.length; i++) {
+                        let part = convert(value[i], nesting + 1, visited);
                         count += (part.string && part.string.length || part.count || 0) + 2;
                         retval.items.push(part);
                     }
@@ -170,9 +171,9 @@ function convert(value, nesting, visited) {
                     retval.string = "{...}";
                 } else {
                     retval.items = [];
-                    var keys = Object.keys(value);
+                    let keys = Object.keys(value);
                     count = 0;
-                    for (i = 0; i < keys.length; i++) {
+                    for (let i = 0; i < keys.length; i++) {
                         part = convert(value[keys[i]], nesting + 1, visited);
                         count += String(keys[i]).length + 4;
                         count += part.string && part.string.length || part.count || 0;
@@ -193,48 +194,50 @@ function convert(value, nesting, visited) {
             break;
     }
     return retval;
-}
+};
 
 /**
  * @param {Object} value
  * @param {Stream} writer
  */
-function printResult(value, writer) {
+exports.printResult = (value, writer) => {
     if (typeof value !== "undefined") {
         writer = writer || term;
         printValue(convert(value, 0, []), writer, 0);
         writer.writeln();
     }
-}
+};
 
-function printValue(value, writer, nesting) {
+const printValue = (value, writer, nesting) => {
     if (value.string) {
-        var style = styles[value.type] || "";
+        const style = styles[value.type] || "";
         writer.write(style + value.string + term.RESET);
     } else if (value && value.items) {
-        var multiline = value.count > 60;
-        var isArray = value.type === "array";
-        var length = value.items.length;
+        const multiline = value.count > 60;
+        const isArray = value.type === "array";
+        const length = value.items.length;
         if (length === 0) {
             writer.write(isArray ? "[]" : "{}");
             return;
         }
-        var opener = isArray ? "[" : "{";
+        const opener = isArray ? "[" : "{";
         if (multiline && nesting > 0) {
             writer.write(opener + "\n  ");
-            for (j = 0; j < nesting; j++)
+            for (let j = 0; j < nesting; j++) {
                 writer.write("  ");
+            }
         } else {
             writer.write(opener + " ");
         }
         if (isArray) {
-            for (var i = 0; i < length; i++) {
+            for (let i = 0; i < length; i++) {
                 printValue(value.items[i], writer, nesting + 1);
                 if (i < length - 1) {
                     if (multiline) {
                         writer.write(",\n  ");
-                        for (var j = 0; j < nesting; j++)
+                        for (let j = 0; j < nesting; j++) {
                             writer.write("  ");
+                        }
                     } else {
                         writer.write(", ");
                     }
@@ -242,14 +245,15 @@ function printValue(value, writer, nesting) {
             }
             writer.write(term.RESET + " ]");
         } else {
-            for (i = 0; i < length; i++) {
+            for (let i = 0; i < length; i++) {
                 writer.write(value.items[i].key);
                 printValue(value.items[i].value, writer, nesting + 1);
                 if (i < length - 1) {
                     if (multiline) {
                         writer.write(",\n  ");
-                        for (j = 0; j < nesting; j++)
+                        for (let j = 0; j < nesting; j++) {
                             writer.write("  ");
+                        }
                     } else {
                         writer.write(", ");
                     }
@@ -258,22 +262,20 @@ function printValue(value, writer, nesting) {
             writer.write(term.RESET + " }");
         }
     }
-}
+};
 
 /**
  * @param {Exception} xcept
  * @param {Array} errors
  * @param {Boolean} verbose
  */
-function printError(xcept, errors, verbose) {
+exports.printError = (xcept, errors, verbose) => {
     if (xcept instanceof org.mozilla.javascript.RhinoException) {
         term.writeln(term.BOLD, term.RED, xcept.details());
     } else {
         term.writeln(term.BOLD, term.RED, xcept.toString());
     }
-    for (var i = 0, l = errors.length; i < l; i++) {
-        term.writeln(term.GREEN, errors[i]);
-    }
+    errors.forEach(error => term.writeln(term.GREEN, error));
     if (typeof xcept.getScriptStackTrace === "function") {
         term.write(xcept.getScriptStackTrace());
     }
@@ -284,13 +286,4 @@ function printError(xcept, errors, verbose) {
         term.writeln(term.BOLD, "Java Exception:")
         xcept.printStackTrace(system.stdout.raw || System.out);
     }
-}
-
-module.exports.write = write;
-module.exports.writeln = writeln;
-module.exports.read = read;
-module.exports.readln = readln;
-module.exports.start = start;
-module.exports.quit = quit;
-module.exports.printResult = printResult;
-module.exports.printError = printError;
+};
