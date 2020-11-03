d406b5fb14a32fb08fa62b0edeb47a69ead209a2
Robert Gaggl <robert.gaggl@orf.at>
2020-11-03 10:51:30 +0100

    stack traces now exclude the test module too, code modernization

--- modules/ringo/utils/test.js
+++ modules/ringo/utils/test.js
@@ -1,21 +1,23 @@
-var strings = require("ringo/utils/strings");
+const strings = require("ringo/utils/strings");
+
+const indent = (lvl) => strings.repeat("    ", lvl);
+const quote = (str) => JSON.stringify(str.toString());
 
 /**
  * Converts the value passed as argument into a nicely formatted and
  * indented string
  * @param {Object} value The value to convert into a string
  * @param {Number} lvl Optional indentation level (defaults to zero)
- * @returns The string representation of the object passed as argument
- * @type String
+ * @returns {String} The string representation of the object passed as argument
  */
-const jsDump = exports.jsDump = function jsDump(value, lvl) {
+const jsDump = exports.jsDump = (value, lvl) => {
     if (!lvl) {
         lvl = 0;
     }
-
+    let buf;
     switch (getType(value)) {
         case "string":
-            return jsDump.quote(value);
+            return quote(value);
         case "boolean":
         case "number":
         case "nan":
@@ -31,41 +33,26 @@ const jsDump = exports.jsDump = function jsDump(value, lvl) {
             }
             return value.toSource();
         case "array":
-            var buf = value.map(function(val) {
-                return jsDump.indent(lvl + 1) + jsDump(val, lvl + 1);
-            });
-            return ["[", buf.join(",\n"), jsDump.indent(lvl) + "]"].join("\n");
+            buf = value.map(val => indent(lvl + 1) + jsDump(val, lvl + 1));
+            return ["[", buf.join(",\n"), indent(lvl) + "]"].join("\n");
         case "object":
-            var buf = [];
-            for (var propName in value) {
-                buf.push(jsDump.indent(lvl + 1) + '"' + propName + '": ' + jsDump(value[propName], lvl + 1));
-            }
-            return ["{", buf.join(",\n"), jsDump.indent(lvl) + "}"].join("\n");
+            buf = Object.keys(value).map(key => {
+                return indent(lvl + 1) + '"' +
+                    key + '": ' +
+                    jsDump(value[key], lvl + 1);
+            });
+            return ["{", buf.join(",\n"), indent(lvl) + "}"].join("\n");
         case "java":
             return '<java:' + value.class.name + '>';
     }
-}
-/**
- * @ignore
- */
-jsDump.indent = function(lvl) {
-    return strings.repeat("    ", lvl);
-};
-
-/**
- * @ignore
- */
-jsDump.quote = function(str) {
-    return JSON.stringify(str.toString());
 };
 
 /**
  * Returns the type of the object passed as argument.
  * @param {Object} obj
- * @returns The type of the object passed as argument
- * @type String
+ * @returns {String} The type of the object passed as argument
  */
-const getType = exports.getType = function getType(obj) {
+const getType = exports.getType = (obj) => {
     if (typeof(obj) === "string") {
         return "string";
     } else if (typeof(obj) === "boolean") {
@@ -88,34 +75,30 @@ const getType = exports.getType = function getType(obj) {
         return "java";
     }
     return "object";
-}
+};
 
 /**
  * Creates a stack trace and parses it for display.
- * @param {java.lang.StackTraceElement} trace The trace to parse. If not given
- * a stacktrace will be generated
- * @returns The parsed stack trace
- * @type String
+ * @param {java.lang.StackTraceElement} trace Optional stacktrace to parse.
+ * If undefined a stacktrace will be generated
+ * @returns {String} The parsed stack trace
  */
-const getStackTrace = exports.getStackTrace = function getStackTrace(trace) {
+exports.getStackTrace = (trace) => {
     // create exception and fill in stack trace
     if (!trace) {
-        var ex = new Packages.org.mozilla.javascript.EvaluatorException("");
+        const ex = new org.mozilla.javascript.EvaluatorException("");
         ex.fillInStackTrace();
         trace = ex.getScriptStack();
     }
-    var stack = [];
-    var el, fileName, lineNumber;
-    for (var i = 0; i < trace.length; i += 1) {
-        el = trace[i];
+    return trace.reduce((stack, el) => {
         if (el.fileName != null && el.lineNumber > -1) {
             // exclude all lines containing the unittest module itself
-            // FIXME (ro): this is quite ugly, but it works ...
-            if (el.fileName.indexOf(module.id) === 0 || el.fileName.indexOf("assert") === 0) {
-                continue;
+            if (!el.fileName.startsWith(module.id) &&
+                    !el.fileName.startsWith("assert.js") &&
+                    el.fileName !== "test.js") {
+                stack.push("at " + el.fileName + ":" + el.lineNumber);
             }
-            stack.push("at " + el.fileName + ":" + el.lineNumber);
         }
-    }
-    return stack;
+        return stack;
+    }, []);
 };
