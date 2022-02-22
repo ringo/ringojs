a9dfd18d474a59fb64be1031b74fc7de9b8bd16d
Robert Gaggl <robert@nomatic.org>
2022-02-22 15:31:11 +0100

    removed debug output

--- test/integration-tests/jars-to-classpath/main.js
+++ test/integration-tests/jars-to-classpath/main.js
@@ -1,7 +1,5 @@
 include('ringo/shell');
 
-java.lang.System.out.println(java.lang.System.getProperty("user.dir"));
-
 var cptester = require("cptester");
 
 if (cptester.printFoo() === "Static method: Foo" && cptester.printBar() === "Public method: Bar") {
