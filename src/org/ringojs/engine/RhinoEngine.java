856775d468942b6c78eac868d5c1b75ea70ec851
Philipp Naderer-Puiu <philipp@naderer-puiu.at>
2022-05-11 14:41:08 +0200

    Bumps version to 4.0.0

--- src/org/ringojs/engine/RhinoEngine.java
+++ src/org/ringojs/engine/RhinoEngine.java
@@ -76,7 +76,7 @@ public class RhinoEngine implements ScopeProvider {
     private static final Logger log = Logger.getLogger(RhinoEngine.class.getName());
 
     public static final List<Integer> VERSION =
-            Collections.unmodifiableList(Arrays.asList(3, 1, 0));
+            Collections.unmodifiableList(Arrays.asList(4, 0, 0));
 
     /**
      * Create a RhinoEngine with the given configuration. If <code>globals</code>
